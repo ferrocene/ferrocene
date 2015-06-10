2cf4324df1f2cf4aa70d107fff2202c610673b17
Alex Crichton <alex@alexcrichton.com>
2015-06-09 23:04:41 -0700

    Test against demangling

--- tests/smoke.rs
+++ tests/smoke.rs
@@ -26,12 +26,12 @@ fn smoke() {
             println!("{:?} {:?} {:#x}", ip, sym, test as usize);
         }
 
-        assert_frame(v[0], backtrace::trace as usize, "trace");
-        assert_frame(v[1], test as usize, "test");
-        assert_frame(v[2], c as usize, "a");
-        assert_frame(v[3], b as usize, "b");
-        assert_frame(v[4], a as usize, "a");
-        assert_frame(v[5], smoke as usize, "smoke");
+        assert_frame(v[0], backtrace::trace as usize, "::trace::");
+        assert_frame(v[1], test as usize, "::test::");
+        assert_frame(v[2], c as usize, "::c::");
+        assert_frame(v[3], b as usize, "::b::");
+        assert_frame(v[4], a as usize, "::a::");
+        assert_frame(v[5], smoke as usize, "smoke::");
     }
 
     fn assert_frame((ip, sym): (*mut libc::c_void, *mut libc::c_void),
@@ -42,10 +42,18 @@ fn smoke() {
         assert!(ip >= sym);
         assert!(sym >= actual_fn_pointer);
         assert!(sym - actual_fn_pointer < 1024);
-        backtrace::resolve(sym as *mut libc::c_void, &mut |sym| {
+        let outer_sym = sym as *mut libc::c_void;
+        backtrace::resolve(outer_sym, &mut |sym| {
             if let Some(bytes) = sym.name() {
                 let bytes = str::from_utf8(bytes).unwrap();
-                assert!(bytes.contains(name));
+                let mut demangled = String::new();
+                backtrace::demangle(&mut demangled, bytes).unwrap();;
+                assert!(demangled.contains(name),
+                        "didn't find `{}` in `{}`", name, demangled);
+            }
+
+            if let Some(addr) = sym.addr() {
+                assert!(outer_sym as usize >= addr as usize);
             }
         });
     }

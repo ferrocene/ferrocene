e99c8f7ca0251f255be2867286647be572431ae9
Alex Crichton <alex@alexcrichton.com>
2015-06-12 01:26:02 -0700

    Don't call any callbacks for noop implementations

--- src/symbolize/noop.rs
+++ src/symbolize/noop.rs
@@ -2,11 +2,6 @@ use libc::c_void;
 
 use Symbol;
 
-struct Dummy;
-
-impl Symbol for Dummy {}
-
-pub fn resolve(_addr: *mut c_void, cb: &mut FnMut(&Symbol)) {
-    cb(&Dummy)
+pub fn resolve(_addr: *mut c_void, _cb: &mut FnMut(&Symbol)) {
 }
 

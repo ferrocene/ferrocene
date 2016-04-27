42f391aead53ab351f6873b042d969c965f9692f
Alex Crichton <alex@alexcrichton.com>
2016-04-26 23:06:17 -0700

    Set up cross-compilation in backtrace-sys

--- backtrace-sys/build.rs
+++ backtrace-sys/build.rs
@@ -1,4 +1,7 @@
+extern crate gcc;
+
 use std::env;
+use std::ffi::OsString;
 use std::path::PathBuf;
 use std::process::Command;
 
@@ -6,20 +9,42 @@ fn main() {
     let src = env::current_dir().unwrap();
     let dst = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     let target = env::var("TARGET").unwrap();
+    let host = env::var("HOST").unwrap();
 
     // libbacktrace doesn't currently support Mach-O files
-    if target.contains("darwin") { return }
-    // libbacktrace isn't used on windows
-    if target.contains("windows") { return }
-    // libbacktrace is already included in the linux libstd for rust
-    if target.contains("linux") { return }
+    if target.contains("darwin") {
+        return
+    }
 
+    // libbacktrace isn't used on windows
+    if target.contains("windows") {
+        return
+    }
+
+    // libbacktrace is already included in the linux libstd for rust
+    if target.contains("linux") {
+        return
+    }
+
+    let cfg = gcc::Config::new();
+    let compiler = cfg.get_compiler();
+    let mut flags = OsString::new();
+    for (i, flag) in compiler.args().iter().enumerate() {
+        if i > 0 {
+            flags.push(" ");
+        }
+        flags.push(flag);
+    }
     run(Command::new(src.join("src/libbacktrace/configure"))
                 .current_dir(&dst)
+                .env("CC", compiler.path())
+                .env("CFLAGS", flags)
                 .arg("--with-pic")
                 .arg("--disable-multilib")
                 .arg("--disable-shared")
-                .arg("--disable-host-shared"));
+                .arg("--disable-host-shared")
+                .arg(format!("--target={}", target))
+                .arg(format!("--host={}", host)));
     run(Command::new("make")
                 .current_dir(&dst)
                 .arg(format!("INCDIR={}",

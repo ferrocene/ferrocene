390ea2a3b630d5bcb83b02c8c6ce814b9d5df7d8
Alex Crichton <alex@alexcrichton.com>
2017-02-02 18:14:58 -0800

    Prefix our own symbols we build

--- backtrace-sys/build.rs
+++ backtrace-sys/build.rs
@@ -2,8 +2,16 @@ extern crate gcc;
 
 use std::env;
 use std::ffi::OsString;
+use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
+
+macro_rules! t {
+    ($e:expr) => (match $e {
+        Ok(e) => e,
+        Err(e) => panic!("{} failed with {}", stringify!($e), e),
+    })
+}
 
 fn main() {
     let src = env::current_dir().unwrap();
@@ -18,11 +26,6 @@ fn main() {
 
     // libbacktrace isn't used on windows
     if target.contains("windows") {
-        return
-    }
-
-    // libbacktrace is already included in the linux libstd for rust
-    if target.contains("linux") {
         return
     }
 
@@ -56,6 +59,29 @@ fn main() {
                              src.join("src/libbacktrace").display())));
     println!("cargo:rustc-link-search=native={}/.libs", dst.display());
     println!("cargo:rustc-link-lib=static=backtrace");
+
+    // The standard library currently bundles in libbacktrace, but it's
+    // compiled with hidden visibility (naturally) so we can't use it.
+    //
+    // To prevent conflicts with a second statically linked copy we rename all
+    // symbols with a '__rbt_' prefix manually here through `objcopy`.
+    let lib = dst.join(".libs/libbacktrace.a");
+    let tmpdir = dst.join("__tmp");
+    drop(fs::remove_dir_all(&tmpdir));
+    t!(fs::create_dir_all(&tmpdir));
+    run(Command::new("ar").arg("x").arg(&lib).current_dir(&tmpdir));
+
+    t!(fs::remove_file(&lib));
+    let mut objs = Vec::new();
+    for obj in t!(tmpdir.read_dir()) {
+        let obj = t!(obj);
+        run(Command::new("objcopy")
+                    .arg("--redefine-syms=symbol-map")
+                    .arg(obj.path()));
+        objs.push(obj.path());
+    }
+
+    run(Command::new("ar").arg("crus").arg(&lib).args(&objs));
 }
 
 fn run(cmd: &mut Command) {
--- backtrace-sys/src/lib.rs
+++ backtrace-sys/src/lib.rs
@@ -24,15 +24,18 @@ pub type backtrace_error_callback =
 pub enum backtrace_state {}
 
 extern {
+    #[link_name = "__rbt_backtrace_create_state"]
     pub fn backtrace_create_state(filename: *const c_char,
                                   threaded: c_int,
                                   error: backtrace_error_callback,
                                   data: *mut c_void) -> *mut backtrace_state;
+    #[link_name = "__rbt_backtrace_syminfo"]
     pub fn backtrace_syminfo(state: *mut backtrace_state,
                              addr: uintptr_t,
                              cb: backtrace_syminfo_callback,
                              error: backtrace_error_callback,
                              data: *mut c_void) -> c_int;
+    #[link_name = "__rbt_backtrace_pcinfo"]
     pub fn backtrace_pcinfo(state: *mut backtrace_state,
                             addr: uintptr_t,
                             cb: backtrace_full_callback,

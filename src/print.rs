136d32dfcb9b03538f33386ff78a605785ee554c
Alex Crichton <alex@alexcrichton.com>
2019-09-04 07:02:04 -0700

    Touch up some documentation in the print module

--- src/print.rs
+++ src/print.rs
@@ -7,7 +7,11 @@ const HEX_WIDTH: usize = 2 + 2 * core::mem::size_of::<usize>();
 #[cfg(target_os = "fuchsia")]
 mod fuchsia;
 
-/// A formatter for  backtraces.
+/// A formatter for backtraces.
+///
+/// This type can be used to print a backtrace regardless of where the backtrace
+/// itself comes from. If you have a `Backtrace` type then its `Debug`
+/// implementation already uses this printing format.
 pub struct BacktraceFmt<'a, 'b> {
     fmt: &'a mut fmt::Formatter<'b>,
     frame_index: usize,
@@ -26,7 +30,13 @@ pub enum PrintFmt {
 }
 
 impl<'a, 'b> BacktraceFmt<'a, 'b> {
-    /// Create a new `BacktraceFmt` which will write output to the provided `fmt`.
+    /// Create a new `BacktraceFmt` which will write output to the provided
+    /// `fmt`.
+    ///
+    /// The `format` argument will control the style in which the backtrace is
+    /// printed, and the `print_path` argument will be used to print the
+    /// `BytesOrWideString` instances of filenames. This type itself doesn't do
+    /// any printing of filenames, but this callback is required to do so.
     pub fn new(
         fmt: &'a mut fmt::Formatter<'b>,
         format: PrintFmt,
@@ -40,9 +50,11 @@ impl<'a, 'b> BacktraceFmt<'a, 'b> {
         }
     }
 
-    /// Adds the current shared library context to the output.
+    /// Prints a preamble for the backtrace about to be printed.
     ///
-    /// This is required for the resulting frames to be symbolized.
+    /// This is required on some platforms for backtraces to be fully
+    /// sumbolicated later, and otherwise this should just be the first method
+    /// you call after creating a `BacktraceFmt`.
     pub fn add_context(&mut self) -> fmt::Result {
         self.fmt.write_str("stack backtrace:\n")?;
         #[cfg(target_os = "fuchsia")]
@@ -51,21 +63,30 @@ impl<'a, 'b> BacktraceFmt<'a, 'b> {
     }
 
     /// Adds a frame to the backtrace output.
-    pub fn frame(&mut self) -> Result<FrameFmt<'_, 'a, 'b>, fmt::Error> {
-        Ok(FrameFmt {
+    ///
+    /// This commit returns an RAII instance of a `FrameFmt` which can be used
+    /// to actually print a frame, and on destruction it will increment the
+    /// frame counter.
+    pub fn frame(&mut self) -> FrameFmt<'_, 'a, 'b> {
+        FrameFmt {
             fmt: self,
             symbol_index: 0,
-        })
+        }
     }
 
     /// Completes the backtrace output.
+    ///
+    /// This is currently a no-op but is added for future compatibility with
+    /// backtrace formats.
     pub fn finish(&mut self) -> fmt::Result {
         // Currently a no-op-- including this hook to allow for future additions.
         Ok(())
     }
 }
 
-/// A formatter for  backtraces.
+/// A formatter for just one frame of a backtrace.
+///
+/// This type is created by the `BacktraceFmt::frame` function.
 pub struct FrameFmt<'fmt, 'a, 'b> {
     fmt: &'fmt mut BacktraceFmt<'a, 'b>,
     symbol_index: usize,
@@ -98,6 +119,9 @@ impl FrameFmt<'_, '_, '_> {
         self.print_raw(
             frame.ip(),
             symbol.name(),
+            // TODO: this isn't great that we don't end up printing anything
+            // with non-utf8 filenames. Thankfully almost everything is utf8 so
+            // this shouldn't be too too bad.
             symbol
                 .filename()
                 .and_then(|p| Some(BytesOrWideString::Bytes(p.to_str()?.as_bytes()))),
@@ -118,7 +142,11 @@ impl FrameFmt<'_, '_, '_> {
         Ok(())
     }
 
-    /// Adds a frame to the backtrace output.
+    /// Adds a raw frame to the backtrace output.
+    ///
+    /// This method, unlike the previous, takes the raw arguments in case
+    /// they're being source from different locations. Note that this may be
+    /// called multiple times for one frame.
     pub fn print_raw(
         &mut self,
         frame_ip: *mut c_void,
@@ -126,6 +154,9 @@ impl FrameFmt<'_, '_, '_> {
         filename: Option<BytesOrWideString>,
         lineno: Option<u32>,
     ) -> fmt::Result {
+        // Fuchsia is unable to symbolize within a process so it has a special
+        // format which can be used to symbolize later. Print that instead of
+        // printing addresses in our own format here.
         if cfg!(target_os = "fuchsia") {
             self.print_raw_fuchsia(frame_ip)?;
         } else {
@@ -160,6 +191,9 @@ impl FrameFmt<'_, '_, '_> {
                 usize::wrapping_sub(frame_ip, std::os::fortanix_sgx::mem::image_base() as _) as _;
         }
 
+        // Print the index of the frame as well as the optional instruction
+        // pointer of the frame. If we're beyond the first symbol of this frame
+        // though we just print appropriate whitespace.
         if self.symbol_index == 0 {
             write!(self.fmt.fmt, "{:4}: ", self.fmt.frame_index)?;
             if let PrintFmt::Full = self.fmt.format {
@@ -172,6 +206,9 @@ impl FrameFmt<'_, '_, '_> {
             }
         }
 
+        // Next up write out the symbol name, using the alternate formatting for
+        // more information if we're a full backtrace. Here we also handle
+        // symbols which don't have a name,
         match (symbol_name, &self.fmt.format) {
             (Some(name), PrintFmt::Short) => write!(self.fmt.fmt, "{}", name)?,
             (Some(name), PrintFmt::Full) => write!(self.fmt.fmt, "{:#}", name)?,
@@ -179,6 +216,7 @@ impl FrameFmt<'_, '_, '_> {
         }
         self.fmt.fmt.write_str("\n")?;
 
+        // And last up, print out the filename/line number if they're available.
         if let (Some(file), Some(line)) = (filename, lineno) {
             self.print_fileline(file, line)?;
         }
@@ -187,11 +225,15 @@ impl FrameFmt<'_, '_, '_> {
     }
 
     fn print_fileline(&mut self, file: BytesOrWideString, line: u32) -> fmt::Result {
-        match self.fmt.format {
-            PrintFmt::Full => write!(self.fmt.fmt, "{:1$}", "", HEX_WIDTH)?,
-            PrintFmt::Short | PrintFmt::__Nonexhaustive => {}
+        // Filename/line are printed on lines under the symbol name, so print
+        // some appropriate whitespace to sort of right-align ourselves.
+        if let PrintFmt::Full = self.fmt.format {
+            write!(self.fmt.fmt, "{:1$}", "", HEX_WIDTH)?;
         }
         write!(self.fmt.fmt, "             at ")?;
+
+        // Delegate to our internal callback to print the filename and then
+        // print out the line number.
         (self.fmt.print_path)(self.fmt.fmt, file)?;
         write!(self.fmt.fmt, ":{}\n", line)?;
         Ok(())

f82329098c975f05edfb6932634bca4307f5092e
Tobias Kortkamp <t6@users.noreply.github.com>
2020-03-24 14:04:19 +0000

    Define HAVE_KERN_PROC on FreeBSD to fix rust-lang/rust#54434 (#294)
    * Define HAVE_KERN_PROC on FreeBSD to fix rust-lang/rust#54434
    
    For the same reason define HAVE_KERN_PROC_ARGS on NetBSD.
    
    This depends on having [1] available in libbacktrace.
    
    [1] https://github.com/ianlancetaylor/libbacktrace/commit/0f06cda953cc4e26f38751c5b9f15ae8dfa5ff2d
    
    * Add dragonfly too

--- crates/backtrace-sys/build.rs
+++ crates/backtrace-sys/build.rs
@@ -72,6 +72,12 @@ fn main() {
     File::create(out_dir.join("config.h")).unwrap();
     if target.contains("android") {
         maybe_enable_dl_iterate_phdr_android(&mut build);
+    } else if target.contains("dragonfly") || target.contains("freebsd") {
+        build.define("HAVE_DL_ITERATE_PHDR", "1");
+        build.define("HAVE_KERN_PROC", "1");
+    } else if target.contains("netbsd") {
+        build.define("HAVE_DL_ITERATE_PHDR", "1");
+        build.define("HAVE_KERN_PROC_ARGS", "1");
     } else if !target.contains("apple-ios")
         && !target.contains("solaris")
         && !target.contains("redox")

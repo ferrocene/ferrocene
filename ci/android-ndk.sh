caf2f0b7593ade9e521fdc73f0b26bbb3b437493
Chris Wailes <chriswailes@google.com>
2023-06-28 10:46:11 -0700

    Update CI script to handle newer versions of the Android NDK

--- ci/android-ndk.sh
+++ ci/android-ndk.sh
@@ -1,23 +1,14 @@
 set -ex
 
-ANDROID_ARCH=$1
-ANDROID_SDK_VERSION=4333796
+ANDROID_NDK_URL=https://dl.google.com/android/repository
+ANDROID_NDK_ARCHIVE=android-ndk-r25b-linux.zip
 
-mkdir /tmp/android
-cd /tmp/android
-
-curl -o android-sdk.zip \
-  "https://dl.google.com/android/repository/sdk-tools-linux-${ANDROID_SDK_VERSION}.zip"
-unzip -q android-sdk.zip
-
-yes | ./tools/bin/sdkmanager --licenses > /dev/null
-./tools/bin/sdkmanager ndk-bundle > /dev/null
-
-./ndk-bundle/build/tools/make_standalone_toolchain.py \
-  --arch $ANDROID_ARCH \
-  --stl=libc++ \
-  --api 21 \
-  --install-dir /android-toolchain
+mkdir /android-toolchain
+cd /android-toolchain
+curl -fO $ANDROID_NDK_URL/$ANDROID_NDK_ARCHIVE
+unzip -q $ANDROID_NDK_ARCHIVE
+rm $ANDROID_NDK_ARCHIVE
+mv android-ndk-* ndk
 
 cd /tmp
 rm -rf android

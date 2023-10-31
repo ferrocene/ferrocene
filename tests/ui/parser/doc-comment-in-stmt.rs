a39884b367ca63d2824f1c436187bfd9b94ff814
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2023-10-31 19:01:02 +0200

    fix upstream-pull conflict

--- tests/ui/parser/doc-comment-in-stmt.rs
+++ tests/ui/parser/doc-comment-in-stmt.rs
@@ -20,15 +20,11 @@ fn quux() -> i32 {
     //~^ ERROR found doc comment
 }
 
-<<<<<<< HEAD
-fn main() {}
-
-// ferrocene-annotations: fls_q8l2jza7d9xa
-// Comments
-=======
 fn main() {
     let x = 0;
     let y = x.max(1) //!foo //~ ERROR found doc comment
         .min(2);
 }
->>>>>>> pull-upstream-temp--do-not-use-for-real-code
+
+// ferrocene-annotations: fls_q8l2jza7d9xa
+// Comments

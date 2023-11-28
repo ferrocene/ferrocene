4c29418eaf294aa681b838b34cd792dc3e348711
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2023-11-28 15:01:36 +0200

    fix upstream pull conflict

--- tests/ui/parser/missing_right_paren.rs
+++ tests/ui/parser/missing_right_paren.rs
@@ -1,8 +1,4 @@
+// ignore-tidy-trailing-newlines
 // error-pattern: this file contains an unclosed delimiter
-<<<<<<< HEAD
-// error-pattern: aborting due to previous error
-fn main((ؼ
-=======
 // error-pattern: aborting due to 1 previous error
 fn main((ؼ
->>>>>>> pull-upstream-temp--do-not-use-for-real-code

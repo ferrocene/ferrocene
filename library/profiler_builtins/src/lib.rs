8a3956f82ab13fb57a1c387af3cf4698e5a78b67
Atri Sarkar <atri.sarkar@Atris-MacBook-Pro.local>
2023-12-04 15:33:24 +0530

    Make profiler_builtins dependency on core optional
    Co-authored-by: Gnana Ganesh <gnana.ganesh@thoughtworks.com>
    Co-authored-by: Amit Dharmapurikar <amit.dharmapurikar@thoughtworks.com>

--- library/profiler_builtins/src/lib.rs
+++ library/profiler_builtins/src/lib.rs
@@ -1,4 +1,6 @@
-#![no_std]
+#![cfg_attr(feature = "core", no_std)]
+#![cfg_attr(not(feature = "core"), feature(no_core), no_core)]
+
 #![feature(profiler_runtime)]
 #![profiler_runtime]
 #![unstable(

d0c51d3397f964454b6ac5c9777a9141f9285de2
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2023-12-08 11:05:11 +0200

    fix upstream pull conflict

--- library/std/build.rs
+++ library/std/build.rs
@@ -34,11 +34,8 @@ fn main() {
         || target.contains("xous")
         || target.contains("hurd")
         || target.contains("uefi")
-<<<<<<< HEAD
-        || target.contains("ferrocenecoretest")
-=======
         || target.contains("teeos")
->>>>>>> pull-upstream-temp--do-not-use-for-real-code
+        || target.contains("ferrocenecoretest")
         // See src/bootstrap/synthetic_targets.rs
         || env::var("RUSTC_BOOTSTRAP_SYNTHETIC_TARGET").is_ok()
     {

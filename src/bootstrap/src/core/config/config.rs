2cb76718d0a8ecf20d848b54e74022c98eb83c2f
Atri Sarkar <atri.sarkar@Atris-MacBook-Pro.local>
2023-12-19 15:57:22 +0530

    Add custom profiler_builtins build step
    Co-authored-by: Amit Dharmapurikar <amit.dharmapurikar@thoughtworks.com>
    Co-authored-by: Gnana Ganesh <gnana.ganesh@thoughtworks.com>

--- src/bootstrap/src/core/config/config.rs
+++ src/bootstrap/src/core/config/config.rs
@@ -345,6 +345,7 @@ pub struct Config {
     pub ferrocene_document_signatures_s3_bucket: String,
     pub ferrocene_ignore_document_signatures: bool,
     pub ferrocene_technical_report_url: Option<String>,
+    pub ferrocene_code_coverage: bool,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -1265,6 +1266,9 @@ impl Config {
         config.llvm_profile_generate = flags.llvm_profile_generate;
         config.enable_bolt_settings = flags.enable_bolt_settings;
         config.bypass_bootstrap_lock = flags.bypass_bootstrap_lock;
+        if let Subcommand::Test { coverage, .. } = &config.cmd {
+            config.ferrocene_code_coverage = *coverage;
+        }
 
         // Infer the rest of the configuration.
 
@@ -1276,11 +1280,13 @@ impl Config {
         // We still support running outside the repository if we find we aren't in a git directory.
         cmd.arg("rev-parse").arg("--show-toplevel");
         // Discard stderr because we expect this to fail when building from a tarball.
-        let output = cmd
-            .stderr(std::process::Stdio::null())
-            .output()
-            .ok()
-            .and_then(|output| if output.status.success() { Some(output) } else { None });
+        let output = cmd.stderr(std::process::Stdio::null()).output().ok().and_then(|output| {
+            if output.status.success() {
+                Some(output)
+            } else {
+                None
+            }
+        });
         if let Some(output) = output {
             let git_root = String::from_utf8(output.stdout).unwrap();
             // We need to canonicalize this path to make sure it uses backslashes instead of forward slashes.
--- src/bootstrap/src/ferrocene/code_coverage.rs
+++ src/bootstrap/src/ferrocene/code_coverage.rs
@@ -1,5 +1,9 @@
 use crate::builder::{Builder, RunConfig, ShouldRun, Step};
-
+use crate::Command;
+use crate::BootstrapCommand;
+use crate::Mode;
+use crate::core::build_steps::tool::SourceType;
+use std::path::{Path, PathBuf};
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, PartialOrd, Ord)]
 pub(crate) struct ProfilerBuiltinsNoCore;
@@ -8,35 +12,45 @@ pub(crate) struct ProfilerBuiltinsNoCore;
 pub(crate) struct ProfilerBuiltins;
 
 impl Step for ProfilerBuiltinsNoCore {
-    type Output = ();
+    type Output = PathBuf;
     const ONLY_HOSTS: bool = true;
     const DEFAULT: bool = true;
 
     fn run(self, builder: &Builder<'_>) -> Self::Output {
         builder.info("Building profiler_builtins with no_core");
-        dbg!("Building profiler_builtins here");
         
-        // let compiler = builder.compiler(0, self.target);
-        // let cargo = tool::prepare_tool_cargo(
-        //     builder,
-        //     compiler,
-        //     Mode::ToolBootstrap,
-        //     self.target,
-        //     "test",
-        //     "ferrocene/tools/generate-tarball",
-        //     SourceType::InTree,
-        //     &[],
-        // );
-        // crate::core::build_steps::test::run_cargo_test(
-        //     cargo,
-        //     &[],
-        //     &[],
-        //     "generate-tarball",
-        //     "generate-tarball",
-        //     compiler,
-        //     self.target,
-        //     builder,
-        // );
+        let target = builder.config.build;
+        let compiler = builder.compiler(1, target);
+
+        // x.py test --coverage
+        // [ProfilerBuiltinNoCore step] Builds profiler_builtins with no core -> profiler_builtins_no_core.rlib
+        // [Core Tests step] Compile tests of core with -Cinsturment-coverage -> link against profiler_builtins_no_core.rlib
+
+        
+        let mut cargo = builder.cargo(compiler, Mode::Std, SourceType::InTree, target, "build");
+        
+        cargo.current_dir(Path::new("library/profiler_builtins"));
+        
+        let profiler_builtins_no_core_dir = "profiler_builtins_no_core";
+        let target_dir =
+        builder.cargo_out(compiler, Mode::Std, target).join(profiler_builtins_no_core_dir);
+
+        cargo.arg("--target-dir");
+        cargo.arg(&*target_dir.to_string_lossy());
+        cargo.arg("--no-default-features");
+        
+        let mut cmd: Command = cargo.into();
+        builder.run_cmd(BootstrapCommand::from(&mut cmd).fail_fast());
+
+        let cargo_dir = if builder.config.rust_optimize.is_release() { "release" } else { "debug" };
+
+        let lib_dir = target_dir.join(&*target.triple).join(cargo_dir);
+        let lib_path = lib_dir.join("libprofiler_builtins.rlib");
+
+        if !builder.config.dry_run() {
+            assert!(lib_path.exists() && lib_path.is_file());
+        }
+        lib_path
     }
 
     fn should_run(run: ShouldRun<'_>) -> ShouldRun<'_> {

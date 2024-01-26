a9c0db9871738afe49f6f4c72fdec44865cb25f3
Pietro Albini <pietro.albini@ferrous-systems.com>
2024-01-26 09:51:39 +0100

    disable genindex to prevent linkchecker errors
    Ferrocene-backport-of: #222

--- ferrocene/doc/qualification-plan/src/conf.py
+++ ferrocene/doc/qualification-plan/src/conf.py
@@ -23,3 +23,8 @@ ferrocene_id = "QP"
 html_theme = "ferrocene"
 html_title = "Ferrocene Qualification Plan"
 html_short_title = "Qualification Plan"
+
+# Do not generate the index pages. We don't need them, and they cause
+# linkchecker to fail due to them including *all* glossary entries, including
+# entries that were removed by autoglossary.
+html_use_index = False
--- ferrocene/doc/safety-manual/src/conf.py
+++ ferrocene/doc/safety-manual/src/conf.py
@@ -24,3 +24,8 @@ ferrocene_id = "SM"
 html_theme = "ferrocene"
 html_title = "Ferrocene Safety Manual"
 html_short_title = "Safety Manual"
+
+# Do not generate the index pages. We don't need them, and they cause
+# linkchecker to fail due to them including *all* glossary entries, including
+# entries that were removed by autoglossary.
+html_use_index = False

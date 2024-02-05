51c0a34e1c2deab65a4be6b5a050862df1f85eaa
Pietro Albini <pietro.albini@ferrous-systems.com>
2024-02-05 15:27:38 +0100

    add table with the summary of each target's test outcomes

---
+++ ferrocene/doc/qualification-report/exts/ferrocene_test_outcomes/render_summary.py
@@ -0,0 +1,121 @@
+# SPDX-License-Identifier: MIT OR Apache-2.0
+# SPDX-FileCopyrightText: The Ferrocene Developers
+
+from .render_template import OutcomesPageNode
+from .utils import RenderTable, paragraph
+from dataclasses import dataclass
+from docutils import nodes
+from sphinx.directives import SphinxDirective
+from sphinx.environment.collectors import EnvironmentCollector
+from sphinx.transforms import SphinxTransform
+from sphinx.util.nodes import make_refnode
+
+
+class PendingSummaryNode(nodes.Element):
+    def __init__(self, location):
+        super().__init__()
+        self.source = location[0]
+        self.line = location[1]
+
+
+class SummaryDirective(SphinxDirective):
+    has_content = False
+
+    def run(self):
+        return [PendingSummaryNode(self.get_source_info())]
+
+
+class InjectSummaryTransform(SphinxTransform):
+    default_priority = 500
+
+    def apply(self):
+        for node in self.document.findall(PendingSummaryNode):
+            self.replace_node(node)
+
+    def replace_node(self, node):
+        pages = get_storage(self.env).copy()
+        pages.sort()
+
+        table = RenderTable(7)
+        table.add_row(
+            paragraph("Host"),
+            paragraph("Target"),
+            paragraph("Results"),
+            paragraph("Passed tests"),
+            paragraph("Failed tests"),
+            paragraph("Ignored tests"),
+            head=True,
+        )
+        for page in pages:
+            platform = self.env.ferrocene_test_outcomes.platform(
+                page.host, page.tested_target
+            )
+
+            def render_sum(field):
+                return str(sum(getattr(invoc, field) for invoc in platform.invocations))
+
+            table.add_row(
+                paragraph(page.host),
+                paragraph(page.target),
+                paragraph(
+                    make_refnode(
+                        self.app.builder,
+                        self.env.docname,
+                        page.document,
+                        "",
+                        nodes.Text("Detailed test results"),
+                    )
+                ),
+                paragraph(render_sum("passed_tests")),
+                paragraph(render_sum("failed_tests")),
+                paragraph(render_sum("ignored_tests")),
+            )
+
+        node.replace_self(table.finalize())
+
+
+class OutcomePagesCollector(EnvironmentCollector):
+    def clear_doc(self, app, env, docname):
+        storage = get_storage(env)
+        storage[:] = [page for page in storage if page.document != docname]
+
+    def merge_other(self, app, env, docnames, other):
+        storage = get_storage(env)
+        other_storage = get_storage(other)
+        for page in other_storage:
+            if page.document in docnames:
+                storage.append(page)
+
+    def process_doc(self, app, document):
+        storage = get_storage(app.env)
+        for node in document.findall(OutcomesPageNode):
+            storage.append(
+                OutcomesPage(
+                    document=app.env.docname,
+                    host=node["host"],
+                    target=node["target"],
+                    tested_target=node["tested_target"],
+                )
+            )
+
+
+def get_storage(env):
+    key = "ferrocene_test_outcome_pages"
+    if not hasattr(env, key):
+        setattr(env, key, [])
+    return getattr(env, key)
+
+
+@dataclass(order=True)
+class OutcomesPage:
+    host: str
+    target: str
+    tested_target: str
+    document: str
+
+
+def setup(app):
+    app.add_node(PendingSummaryNode)
+    app.add_directive("render-summary", SummaryDirective)
+    app.add_post_transform(InjectSummaryTransform)
+    app.add_env_collector(OutcomePagesCollector)

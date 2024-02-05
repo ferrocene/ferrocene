ca5a1f9caa0abdbb8cd67d9410497ac3a4e6f1a2
Pietro Albini <pietro.albini@ferrous-systems.com>
2024-02-05 15:27:38 +0100

    add table with the summary of each target's test outcomes

--- ferrocene/doc/qualification-report/exts/ferrocene_test_outcomes/__init__.py
+++ ferrocene/doc/qualification-report/exts/ferrocene_test_outcomes/__init__.py
@@ -1,11 +1,12 @@
 # SPDX-License-Identifier: MIT OR Apache-2.0
 # SPDX-FileCopyrightText: The Ferrocene Developers
 
-from . import outcomes, render_template
+from . import outcomes, render_template, render_summary
 
 def setup(app):
     outcomes.setup(app)
     render_template.setup(app)
+    render_summary.setup(app)
 
     return {
         "version": "0",

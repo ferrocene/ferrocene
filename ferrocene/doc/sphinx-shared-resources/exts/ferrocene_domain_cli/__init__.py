0702b1a08ff0d975ddb722ee8605832db2dd0ff5
Pietro Albini <pietro.albini@ferrous-systems.com>
2024-02-20 10:20:03 +0100

    prepare the ferrocene_domain_cli extension to have multiple files

---
+++ ferrocene/doc/sphinx-shared-resources/exts/ferrocene_domain_cli/__init__.py
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: MIT OR Apache-2.0
+# SPDX-FileCopyrightText: The Ferrocene Developers
+
+# This extension defines the "cli" domain, which contains the "program" and
+# "option" directives. Those directives are used to document the CLI interface
+# of the binaries we ship.
+#
+# Sphinx *has* builtin "program" and "option" directives, but unfortunately
+# they assume that the name of the command line option is just the one that
+# starts with "-" (for example "--crate-name"), but for our use case we want to
+# consider "-C opt-level=<level>" as the "-C opt-level" argument.
+
+from . import domain
+
+
+def setup(app):
+    domain.setup(app)
+
+    return {
+        "version": "0",
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+        # The version needs to be updated whenever there is a breaking change
+        # in the data stored in the environment. Bumping the version number
+        # will ensure Sphinx will do a clean build.
+        #
+        # Version history:
+        # - 0: initial implementation
+        "env_version": "0",
+    }
--- ferrocene/doc/sphinx-shared-resources/exts/ferrocene_domain_cli/domain.py
+++ ferrocene/doc/sphinx-shared-resources/exts/ferrocene_domain_cli/domain.py
@@ -1,15 +1,6 @@
 # SPDX-License-Identifier: MIT OR Apache-2.0
 # SPDX-FileCopyrightText: The Ferrocene Developers
 
-# This extension defines the "cli" domain, which contains the "program" and
-# "option" directives. Those directives are used to document the CLI interface
-# of the binaries we ship.
-#
-# Sphinx *has* builtin "program" and "option" directives, but unfortunately
-# they assume that the name of the command line option is just the one that
-# starts with "-" (for example "--crate-name"), but for our use case we want to
-# consider "-C opt-level=<level>" as the "-C opt-level" argument.
-
 from docutils import nodes
 from sphinx import addnodes
 from sphinx.directives import SphinxDirective, ObjectDescription
@@ -158,16 +149,3 @@ def warn(message, location):
 
 def setup(app):
     app.add_domain(CliDomain)
-
-    return {
-        "version": "0",
-        "parallel_read_safe": True,
-        "parallel_write_safe": True,
-        # The version needs to be updated whenever there is a breaking change
-        # in the data stored in the environment. Bumping the version number
-        # will ensure Sphinx will do a clean build.
-        #
-        # Version history:
-        # - 0: initial implementation
-        "env_version": "0",
-    }

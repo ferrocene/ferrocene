ccd8a92be3cc17cc992fbb07f29f7547b90520ea
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2024-02-23 08:53:08 +0200

    tidy: reduce diff with upstream

--- src/tools/tidy/src/deps.rs
+++ src/tools/tidy/src/deps.rs
@@ -13,7 +13,7 @@ const LICENSES: &[&str] = &[
     "0BSD OR MIT OR Apache-2.0",                           // adler license
     "0BSD",
     "Apache-2.0 / MIT",
-    "Apache-2.0 OR ISC OR MIT",                            // rustls
+    "Apache-2.0 OR ISC OR MIT",
     "Apache-2.0 OR MIT",
     "Apache-2.0 WITH LLVM-exception OR Apache-2.0 OR MIT", // wasi license
     "Apache-2.0",

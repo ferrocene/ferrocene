76173aa2e76b5f65297d8edf7df455fdff4cdfbe
Johann Hemmann <johann.hemmann@ferrous-systems.com>
2024-03-20 23:22:47 +0100

    Adapt the code (mainly the path) and add it to `get_step_descriptions`

--- src/bootstrap/src/ferrocene/tool/flip_link.rs
+++ src/bootstrap/src/ferrocene/tool/flip_link.rs
@@ -1,5 +1,4 @@
 use std::path::PathBuf;
-use std::process::Command;
 
 use crate::builder::{Builder, RunConfig, ShouldRun, Step};
 use crate::core::build_steps::tool::{prepare_tool_cargo, SourceType};
@@ -11,21 +10,7 @@ pub(crate) struct FlipLink {
     pub(crate) target: TargetSelection,
 }
 
-impl FlipLink {
-    pub(crate) fn update_command(
-        cmd: &mut Command,
-        builder: &Builder<'_>,
-        target: TargetSelection,
-    ) {
-        cmd.env("SELFTEST_TARGET", target.to_string());
-        if let Some(hash) = builder.rust_info().sha() {
-            cmd.env("SELFTEST_RUST_HASH", hash);
-        }
-        if let Some(hash) = builder.cargo_info.sha() {
-            cmd.env("SELFTEST_CARGO_HASH", hash);
-        }
-    }
-}
+const PATH: &str = "ferrocene/tools/flip-link";
 
 impl Step for FlipLink {
     type Output = PathBuf;
@@ -33,7 +18,7 @@ impl Step for FlipLink {
     const ONLY_HOSTS: bool = true;
 
     fn should_run(run: ShouldRun<'_>) -> ShouldRun<'_> {
-        run.path("ferrocene/tools/self-test")
+        run.path(PATH)
     }
 
     fn make_run(run: RunConfig<'_>) {
@@ -41,7 +26,7 @@ impl Step for FlipLink {
     }
 
     fn run(self, builder: &Builder<'_>) -> Self::Output {
-        builder.info("Building ferrocene/tools/self-test");
+        builder.info(format!("Building {PATH}").as_str());
 
         let compiler = builder.compiler(0, builder.config.build);
         let mut cmd = prepare_tool_cargo(
@@ -50,17 +35,16 @@ impl Step for FlipLink {
             Mode::ToolBootstrap,
             self.target,
             "build",
-            "ferrocene/tools/self-test",
-            SourceType::InTree,
+            PATH,
+            SourceType::Submodule,
             &[],
         )
         .into();
-        Self::update_command(&mut cmd, builder, self.target);
 
         builder.run(&mut cmd);
 
         builder
             .cargo_out(compiler, Mode::ToolBootstrap, self.target)
-            .join(exe("ferrocene-self-test", self.target))
+            .join(exe("flip-link", self.target))
     }
 }

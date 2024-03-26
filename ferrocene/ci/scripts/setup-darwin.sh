08af29f2c2f68f0e467693a8d87972f6f64e99bc
Ana Hobden <ana.hobden@ferrous-systems.com>
2024-03-26 12:53:44 -0700

    Add setup-darwin.sh

---
+++ ferrocene/ci/scripts/setup-darwin.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# SPDX-License-Identifier: MIT OR Apache-2.0
+# SPDX-FileCopyrightText: The Ferrocene Developers
+
+# Unlike Windows/Mac executors, Macs do not come with awscli by default
+brew install awscli cmake ninja

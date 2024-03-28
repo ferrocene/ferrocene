a5c0779fc7832992654019d9ec50e1fbb0b8329b
Ana Hobden <ana.hobden@ferrous-systems.com>
2024-03-28 08:12:45 -0700

    Try to debug tar taking forever
    Workaround TSO issue
    
    Move Mac jobs to full workflow

--- ferrocene/ci/scripts/setup-darwin.sh
+++ ferrocene/ci/scripts/setup-darwin.sh
@@ -4,3 +4,9 @@
 
 # Unlike Windows/Mac executors, Macs do not come with awscli by default
 brew install awscli cmake ninja zstd
+
+# There are a number of reports of very slow uploads in Mac VMs due to TSO, disable it.
+# https://github.com/aws/aws-sdk/issues/469
+# https://github.com/cypress-io/cypress/issues/28033#issuecomment-1879102700
+# https://support.circleci.com/hc/en-us/articles/19334402064027-Troubleshooting-slow-uploads-to-S3-for-jobs-using-an-m1-macOS-resource-class
+sudo sysctl net.inet.tcp.tso=0

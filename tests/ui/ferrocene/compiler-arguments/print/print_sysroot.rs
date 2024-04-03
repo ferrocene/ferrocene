3eb4390f0f4d9438d0c6970d02d6cb27a9fd8521
Ana Hobden <ana.hobden@ferrous-systems.com>
2024-04-03 13:48:12 -0700

    Fix some Mac only tests

--- tests/ui/ferrocene/compiler-arguments/print/print_sysroot.rs
+++ tests/ui/ferrocene/compiler-arguments/print/print_sysroot.rs
@@ -5,8 +5,9 @@
 // normalize-stdout-test: "ci-rustc-sysroot" -> "$$STAGE"
 //
 // If this test fails on a new platform, add a new normalization annotation:
-// normalize-stdout-test: "x86_64-unknown-linux-gnu" -> "$$TARGET"
-// normalize-stdout-test: "aarch64-unknown-linux-gnu" -> "$$TARGET"
+//@ normalize-stdout-test: "x86_64-unknown-linux-gnu" -> "$$TARGET"
+//@ normalize-stdout-test: "aarch64-unknown-linux-gnu" -> "$$TARGET"
+//@ normalize-stdout-test: "aarch64-apple-darwin" -> "$$TARGET"
 
 fn main() {}
 
--- tests/ui/ferrocene/compiler-arguments/print/print_target-libdir.rs
+++ tests/ui/ferrocene/compiler-arguments/print/print_target-libdir.rs
@@ -5,9 +5,10 @@
 // normalize-stdout-test: "ci-rustc-sysroot" -> "$$STAGE"
 //
 // If this test fails on a new platform, add a new normalization annotation:
-// normalize-stdout-test: "x86_64-unknown-linux-gnu" -> "$$TARGET"
-// normalize-stdout-test: "aarch64-unknown-linux-gnu" -> "$$TARGET"
-// normalize-stdout-test: "aarch64-unknown-ferrocenecoretest" -> "$$TARGET"
+//@ normalize-stdout-test: "x86_64-unknown-linux-gnu" -> "$$TARGET"
+//@ normalize-stdout-test: "aarch64-unknown-linux-gnu" -> "$$TARGET"
+//@ normalize-stdout-test: "aarch64-unknown-ferrocenecoretest" -> "$$TARGET"
+//@ normalize-stdout-test: "aarch64-apple-darwin" -> "$$TARGET"
 
 fn main() {}
 

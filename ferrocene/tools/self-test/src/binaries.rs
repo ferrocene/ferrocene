192275fffbb737f022ffd9cbdc9c6574e3702258
Ana Hobden <ana.hobden@ferrous-systems.com>
2024-04-04 09:01:51 -0700

    fmt

--- ferrocene/tools/self-test/src/binaries.rs
+++ ferrocene/tools/self-test/src/binaries.rs
@@ -82,7 +82,7 @@ fn parse_version_output(output: &str) -> Option<VersionOutput<'_>> {
             _ => {}
         }
     }
-    
+
     Some(VersionOutput { release: release?, commit_hash: commit_hash?, host: host? })
 }
 

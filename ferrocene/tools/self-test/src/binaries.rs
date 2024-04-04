1deb5ef06bcdce0d4d64534e504e7869244e8110
Ana Hobden <ana.hobden@ferrous-systems.com>
2024-04-04 08:07:45 -0700

    Fix a bad merge conflict

--- ferrocene/tools/self-test/src/binaries.rs
+++ ferrocene/tools/self-test/src/binaries.rs
@@ -88,7 +88,7 @@ fn parse_version_output(output: &str) -> Option<VersionOutput> {
             _ => {}
         }
     }
-    eprintln!("output: {output:?}\nparsed: {release:?} {commit_hash:?} {host:?}");
+    
     Some(VersionOutput { release: release?, commit_hash: commit_hash?, host: host? })
 }
 
--- ferrocene/tools/self-test/src/targets.rs
+++ ferrocene/tools/self-test/src/targets.rs
@@ -20,8 +20,6 @@ static SUPPORTED_TARGETS: &[TargetSpec] = &[
     TargetSpec { triple: "armv8r-none-eabihf", std: false, linker: Linker::BundledLld },
     TargetSpec { triple: "thumbv7em-none-eabihf", std: false, linker: Linker::BundledLld },
     TargetSpec { triple: "thumbv7em-none-eabi", std: false, linker: Linker::BundledLld },
-    TargetSpec { triple: "aarch64-apple-darwin", std: true, linker: Linker::HostCC },
-    TargetSpec { triple: "x86_64-apple-darwin", std: true, linker: Linker::HostCC },
     TargetSpec { triple: "aarch64-apple-darwin", std: true, linker: Linker::BundledLld },
     TargetSpec { triple: "x86_64-apple-darwin", std: true, linker: Linker::BundledLld },
 ];

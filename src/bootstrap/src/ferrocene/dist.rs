b4c80147b90ef6dc9e500010317e2d59b50d4f86
Pietro Albini <pietro.albini@ferrous-systems.com>
2024-04-22 13:05:59 +0200

    vendor dependencies for cargo and rustc_codegen_gcc too

--- src/bootstrap/src/ferrocene/dist.rs
+++ src/bootstrap/src/ferrocene/dist.rs
@@ -115,7 +115,9 @@ impl Step for SourceTarball {
         ];
         const EXTRA_CARGO_TOMLS: &[&str] = &[
             "compiler/rustc_codegen_cranelift/Cargo.toml",
+            "compiler/rustc_codegen_gcc/Cargo.toml",
             "src/bootstrap/Cargo.toml",
+            "src/tools/cargo/Cargo.toml",
             "src/tools/rust-analyzer/Cargo.toml",
         ];
 

d0a2bfb3c5a3d080b9511486f94cb14a80d56478
Ana Hobden <operator@hoverbear.org>
2024-05-03 08:10:40 -0700

    fmt

--- ferrocene/tools/self-test/src/targets.rs
+++ ferrocene/tools/self-test/src/targets.rs
@@ -12,20 +12,20 @@ use crate::report::Reporter;
 static SUPPORTED_TARGETS: &[TargetSpec] = &[
     #[cfg(target_arch = "x86_64")]
     TargetSpec { triple: "x86_64-unknown-linux-gnu", std: true, linker: Linker::HostCc },
-    #[cfg(target_arch = "x86_64")]TargetSpec {
+    #[cfg(target_arch = "x86_64")]
+    TargetSpec {
         triple: "aarch64-unknown-linux-gnu",
         std: true,
         linker: Linker::CrossCc(&["aarch64-linux-gnu-"]),
     },
-
     #[cfg(target_arch = "aarch64")]
     TargetSpec { triple: "aarch64-unknown-linux-gnu", std: true, linker: Linker::HostCc },
-    #[cfg(target_arch = "aarch64")]TargetSpec {
+    #[cfg(target_arch = "aarch64")]
+    TargetSpec {
         triple: "x86_64-unknown-linux-gnu",
         std: true,
-        linker: Linker::CrossCC(&["x86_64-linux-gnu-"]),
+        linker: Linker::CrossCc(&["x86_64-linux-gnu-"]),
     },
-    
     TargetSpec { triple: "aarch64-unknown-none", std: false, linker: Linker::BundledLld },
     TargetSpec { triple: "armv8r-none-eabihf", std: false, linker: Linker::BundledLld },
     TargetSpec { triple: "thumbv7em-none-eabihf", std: false, linker: Linker::BundledLld },

af52eee1d8d7b325e60939d5848c49fcc224e3b9
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2024-05-21 02:08:04 +0200

    fix upstream pull conflicts

--- src/bootstrap/src/core/sanity.rs
+++ src/bootstrap/src/core/sanity.rs
@@ -33,12 +33,7 @@ pub struct Finder {
 // Targets can be removed from this list once they are present in the stage0 compiler (usually by updating the beta compiler of the bootstrap).
 const STAGE0_MISSING_TARGETS: &[&str] = &[
     // just a dummy comment so the list doesn't get onelined
-<<<<<<< HEAD
-    "aarch64-apple-visionos",
-    "aarch64-apple-visionos-sim",
     "aarch64-unknown-ferrocenecoretest",
-=======
->>>>>>> pull-upstream-temp--do-not-use-for-real-code
 ];
 
 impl Finder {

eb6ecfbfba768abe0a3dbaba5e16455fbd0ccafd
Ana Hobden <ana.hobden@ferrous-systems.com>
2024-05-29 11:56:53 -0700

    Remove dead code

--- src/bootstrap/src/ferrocene/install.rs
+++ src/bootstrap/src/ferrocene/install.rs
@@ -5,10 +5,8 @@
 //! This module is responsible for installing the standard library,
 //! compiler, and documentation.
 
-// use std::env;
 use std::fs;
 use std::path::{Path, PathBuf};
-// use std::process::Command;
 
 use walkdir::WalkDir;
 

e1c275947223c4cd83fd50812ab7b8d12859d4a1
Lukas Wirth <lukas.wirth@ferrous-systems.com>
2024-06-07 14:00:16 +0200

    Annotate more proc-macro tests

--- tests/ui/macros/macro-quote-test.rs
+++ tests/ui/macros/macro-quote-test.rs
@@ -8,3 +8,15 @@ extern crate hello_macro;
 fn main() {
     hello_macro::hello!();
 }
+
+// ferrocene-annotations: fls_wjldgtio5o75
+// Macro Expansion
+//
+// ferrocene-annotations: fls_vnvt40pa48n8
+// Macro Invocation
+//
+// ferrocene-annotations: fls_wn1i6hzg2ff7
+// Procedural Macros
+//
+// ferrocene-annotations: fls_2d6bqnpy6tvs
+// Function-like Macros

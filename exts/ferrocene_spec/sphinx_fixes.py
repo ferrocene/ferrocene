71af28074d5258a45fcccc84342a75b8ad8e7f3b
Pietro Albini <pietro.albini@ferrous-systems.com>
2024-07-04 14:55:00 +0200

    fix references to fls sections being broken

--- exts/ferrocene_spec/sphinx_fixes.py
+++ exts/ferrocene_spec/sphinx_fixes.py
@@ -14,15 +14,35 @@ class FerroceneSortIds(SphinxTransform):
     # Run this step after sphinx sorted.
     default_priority = SortIds.default_priority + 1
 
-    def apply(self, **kwargs):
+    def apply(self):
         for node in self.document.findall(nodes.section):
             for n, id in enumerate(node["ids"]):
-                if id.startswith("fls-"):
-                    node["ids"][n] = id[:3] + "_" + id[4:]
+                node["ids"][n] = normalize_fls_id(id)
             # sort the fls id to the back
             if len(node["ids"]) > 1 and node["ids"][0].startswith("fls_"):
                 node["ids"] = node["ids"][1:] + [node["ids"][0]]
 
 
+class NormalizeFlsReferences(SphinxTransform):
+    default_priority = 500
+
+    def apply(self):
+        for reference in self.document.findall(nodes.reference):
+            if "internal" not in reference or not reference["internal"]:
+                continue
+            if "refuri" not in reference or "#" not in reference["refuri"]:
+                continue
+            path, hash = reference["refuri"].rsplit("#", 1)
+            reference["refuri"] = f"{path}#{normalize_fls_id(hash)}"
+
+
+def normalize_fls_id(id):
+    if id.startswith("fls-"):
+        return id.replace("fls-", "fls_", 1)
+    else:
+        return id
+
+
 def setup(app):
     app.add_transform(FerroceneSortIds)
+    app.add_post_transform(NormalizeFlsReferences)

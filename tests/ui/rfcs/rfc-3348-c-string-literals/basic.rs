88246beee2d5bad0f5f7b4b08c07b0574a9ccced
Lukas Wirth <lukas.wirth@ferrous-systems.com>
2024-07-05 18:03:31 +0200

    Annotate C string literal tests

--- tests/ui/rfcs/rfc-3348-c-string-literals/basic.rs
+++ tests/ui/rfcs/rfc-3348-c-string-literals/basic.rs
@@ -4,3 +4,9 @@
 fn main() {
     assert_eq!(b"test\0", c"test".to_bytes_with_nul());
 }
+
+// ferrocene-annotations: fls_u1ghcy16emve
+// C String Literals
+//
+// ferrocene-annotations: fls_p090c5otnelw
+// Simple C String Literals

7e2dd12c92c8efe91e6218b3a2f6756db3233ce3
Ana Hobden <operator@hoverbear.org>
2024-07-19 11:21:53 -0700

    Be very very explicit about which python we use for qnx unpack

--- ferrocene/ci/scripts/setup-venv.sh
+++ ferrocene/ci/scripts/setup-venv.sh
@@ -12,15 +12,15 @@ fi
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
 source $HOME/.cargo/env
-echo "source $HOME/.cargo/env" >> $HOME/.bashrc
+echo "source $HOME/.cargo/env" >> $BASH_ENV
 
 uv venv ~/.venv
 if [[ "${OSTYPE}" = "msys" ]]; then
     source ~/.venv/Scripts/activate
-    echo "source $HOME/.venv/Scripts/activate" >> $HOME/.bashrc
+    echo "source $HOME/.venv/Scripts/activate" >> $BASH_ENV
 else
     source ~/.venv/bin/activate
-    echo "source $HOME/.venv/bin/activate" >> $HOME/.bashrc
+    echo "source $HOME/.venv/bin/activate" >> $BASH_ENV
 fi
 
 uv pip sync requirements.txt

cbb91f93d59ce461f96f760c5e367a66d824a08d
Ana Hobden <operator@hoverbear.org>
2024-08-08 11:10:04 -0700

    Do a bit of brainstorming

--- ferrocene/ci/github-actions/generate.py
+++ ferrocene/ci/github-actions/generate.py
@@ -10,33 +10,76 @@ from pathlib import Path
 from enum import Enum
 
 class TargetTriple:
-    def __init__(self, host: bool, setup_script: Path):
+    def __init__(self, triple: str, host: bool, runner: str = None, setup_script: Path = None):
+        self.triple = triple
         self.host = host
+        self.runner = runner
         self.setup_script = setup_script
+
+    def runner(self):
+        self.runner
+
+    def generate_build_job(self):
+        if not self.host:
+            raise ScriptError(f"{self.triple} is not a host platform.")
+
+        job = {
+            "name": f"{self.triple}-build",
+            "runs_on": self.runner,
+        }
+        return job
+
+    def generate_llvm_job(self):
+        if not self.host:
+            raise ScriptError(f"{self.triple} is not a host platform.")
+            
+        job = {
+            "name": f"{self.triple}-build",
+            "runs_on": self.runner,
+        }
+        return job
+
+    def generate_dist_job(self):
+        if not self.host:
+            raise ScriptError(f"{self.triple} is not a host platform.")
+            
+        job = {
+            "name": f"{self.triple}-build",
+            "runs_on": self.runner,
+        }
+        return job
 
 
 TARGET_TRIPLES = {
     "x86_64-unknown-linux-gnu": TargetTriple(
+        triple="x86_64-unknown-linux-gnu",
         host=True,
-        setup_script=None
+        runner="ubuntu-latest"
     ),
     "aarch64-unknown-linux-gnu": TargetTriple(
+        triple="aarch64-unknown-linux-gnu",
         host=True,
-        setup_script=None
+        runner="TODO"
     ),
     "x86_64-pc-windows-msvc": TargetTriple(
+        triple="x86_64-pc-windows-msvc",
         host=True,
+        runner="windows-latest",
         setup_script="setup-darwin.sh"
     ),
     "aarch64-apple-darwin": TargetTriple(
+        triple="aarch64-apple-darwin",
         host=True,
+        runner="TODO",
         setup_script="setup-windows.sh"
     ),
     "armv8r-none-eabihf": TargetTriple(
+        triple="armv8r-none-eabihf",
         host=False,
         setup_script="setup-arm-toolchain.sh",
     ),
     "armv7r-none-eabihf": TargetTriple(
+        triple="armv7r-none-eabihf",
         host=False,
         setup_script="setup-arm-toolchain.sh",
     ),
@@ -44,13 +87,17 @@ TARGET_TRIPLES = {
 
 
 def generate_workflow():
-    template = yaml.load("ferrocene/ci/github-actions/workflow_template.yaml", Loader=yaml.Loader)
+    template_file = open("ferrocene/ci/github-actions/workflow_template.yaml", "r")
+    template = yaml.load(template_file, Loader=yaml.Loader)
 
+    jobs = template["jobs"]
 
-    for triple in TARGET_TRIPLES:
-        triple_info = TARGET_TRIPLES[triple]
-        if triple_info.host:
-            logging.info(f"{triple}: {triple_info.host}")
+    for target in TARGET_TRIPLES:
+        target = TARGET_TRIPLES[target]
+        if target.host:
+            jobs[f"{target.triple}-llvm"] = target.generate_llvm_job()
+            jobs[f"{target.triple}-build"] = target.generate_build_job()
+            jobs[f"{target.triple}-dist"] = target.generate_dist_job()
     
 
     return template
@@ -82,7 +129,7 @@ def main():
     logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", datefmt="%I:%M:%S %p", level=log_level)
 
     try:
-        print(yaml.dump(generate_workflow()))
+        print(yaml.dump(generate_workflow(), Dumper=yaml.Dumper))
     except ScriptError as e:
         print(f"error: {e}", file=sys.stderr)
         exit(1)

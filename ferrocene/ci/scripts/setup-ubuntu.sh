eeb8f8d2976d641bb48d17d3dce28e5912ad3ca5
Ana Hobden <ana.hobden@ferrous-systems.com>
2024-08-30 10:18:56 -0700

    Don't sudo setup scripts

--- ferrocene/ci/scripts/setup-ubuntu.sh
+++ ferrocene/ci/scripts/setup-ubuntu.sh
@@ -3,27 +3,24 @@
 # SPDX-FileCopyrightText: The Ferrocene Developers
 set -xeo pipefail
 
-# Ensure we never get asked/prompted, always take the new config
-echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
-
 # Sometimes (eg the commit job) we want to skip doing anything time intensive like upgrading.
 # Jobs which actually produce artifacts should *always* upgrade as they may otherwise miss critical
 # security upgrades.
 if [[ ! -z "${SKIP_UPGRADE}" ]]; then
     echo 'Skipping upgrade, $SKIP_UPGRADE is set. Use caution if any artifacts are produced.'
 else
-    apt update
-    apt upgrade -y
+    sudo apt update
+    sudo apt upgrade -y
 fi
 
-apt install -y \
+sudo apt install -y \
     git \
     build-essential \
     ninja-build \
     zlib1g-dev
 
 if [[ ! -z "${INSTALL_LLVM}" ]]; then
-    apt install -y \
+    sudo apt install -y \
         llvm-17-tools \
         llvm-17-dev
 else

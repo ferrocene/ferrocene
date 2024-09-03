298944b97291a447e483302e09b6bd379640fd7f
Ana Hobden <operator@hoverbear.org>
2024-09-03 11:20:30 -0700

    Fixup logic flaws

--- ferrocene/ci/scripts/setup-ubuntu.sh
+++ ferrocene/ci/scripts/setup-ubuntu.sh
@@ -9,7 +9,7 @@ echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 # Sometimes (eg the commit job) we want to skip doing anything time intensive like upgrading.
 # Jobs which actually produce artifacts should *always* upgrade as they may otherwise miss critical
 # security upgrades.
-if [[ -z "${SKIP_UPGRADE}" ]]; then
+if [[ ! -z "${SKIP_UPGRADE}" ]]; then
     echo 'Skipping upgrade, $SKIP_UPGRADE is set. Use caution if any artifacts are produced.'
 else
     apt update
@@ -22,10 +22,10 @@ apt install -y \
     ninja-build \
     zlib1g-dev
 
-if [[ -z "${INSTALL_LLVM}" ]]; then
+if [[ ! -z "${INSTALL_LLVM}" ]]; then
     apt install -y \
         llvm-17-tools \
         llvm-17-dev
 else
-    echo 'Not installing LLVM, $INSTALL_LLVM is not set.'
+    echo 'Not installing LLVM, $INSTALL_LLVM is unset.'
 fi

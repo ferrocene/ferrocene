dd1cc20be214e67564e902fd9e5bc6f94f739ca9
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2024-09-20 17:58:09 +0200

    fix upstream pull conflicts and build fails

--- src/bootstrap/src/core/builder.rs
+++ src/bootstrap/src/core/builder.rs
@@ -14,11 +14,7 @@ use clap::ValueEnum;
 
 use crate::core::build_steps::tool::{self, SourceType};
 use crate::core::build_steps::{
-<<<<<<< HEAD
-    check, clean, clippy, compile, dist, doc, llvm, run, setup, test, vendor,
-=======
-    check, clean, clippy, compile, dist, doc, gcc, install, llvm, run, setup, test, vendor,
->>>>>>> pull-upstream-temp--do-not-use-for-real-code
+    check, clean, clippy, compile, dist, doc, gcc, llvm, run, setup, test, vendor,
 };
 use crate::core::config::flags::{Color, Subcommand};
 use crate::core::config::{DryRun, SplitDebuginfo, TargetSelection};

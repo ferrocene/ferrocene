63fa2f267b4064a1d16045105055fb2160cc11cc
Ana Hobden <operator@hoverbear.org>
2024-11-08 10:12:24 -0800

    Ignore some more windows tests, repair uv usage a bit

--- tests/ui/ferrocene/compiler-arguments/remap-path-prefix/remap-path-prefix_macro-expansion.rs
+++ tests/ui/ferrocene/compiler-arguments/remap-path-prefix/remap-path-prefix_macro-expansion.rs
@@ -2,9 +2,9 @@
 //@ compile-flags: --remap-path-prefix={{src-base}}/ferrocene/compiler-arguments/remap-path-prefix=remapped
 
 fn main() {
-    let this_file = file!();
+    let this_file = std::path::PathBuf::from(file!());
     let expected = std::path::PathBuf::from("remapped/remap-path-prefix_macro-expansion.rs");
-    assert_eq!(expected.as_str(), this_file);
+    assert_eq!(expected, this_file);
 }
 
 // ferrocene-annotations: um_rustc_remap_path_prefix

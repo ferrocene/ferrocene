7fdb6bf2ea83b36f7d6fe62b000e9d25d967b54a
Ana Hobden <operator@hoverbear.org>
2024-11-08 10:12:24 -0800

    Ignore some more windows tests, repair uv usage a bit

--- tests/ui/ferrocene/compiler-arguments/color/color_always.rs
+++ tests/ui/ferrocene/compiler-arguments/color/color_always.rs
@@ -1,6 +1,7 @@
 //@ check-fail
 //@ compile-flags: --error-format human --color always
 //@ error-pattern: cannot find value `x` in this scope
+//@ ignore-windows: Target uses different escape codes.
 
 fn main() {
     let x = x;

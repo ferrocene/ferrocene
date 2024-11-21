8ecdfaf7ec67ff21f6513cbf7d952b82441a907c
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2024-11-21 10:15:41 +0200

    fix upstream pull conflict

--- tests/ui/structs-enums/enum-rec/issue-17431-6.rs
+++ tests/ui/structs-enums/enum-rec/issue-17431-6.rs
@@ -1,10 +1,5 @@
 use std::cell::UnsafeCell;
 
-<<<<<<< HEAD
-use std::cell::UnsafeCell;
-
-=======
->>>>>>> pull-upstream-temp--do-not-use-for-real-code
 enum Foo { X(UnsafeCell<Option<Foo>>) }
 //~^ ERROR recursive type `Foo` has infinite size
 //~| ERROR cycle detected

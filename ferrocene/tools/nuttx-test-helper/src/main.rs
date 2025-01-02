d519e42f0e354554bacd7adbbdedf5e8df84c3c7
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2025-01-02 19:25:40 +0100

    spawner: switch from ? to more verbose panic messages

--- ferrocene/tools/nuttx-test-helper/src/main.rs
+++ ferrocene/tools/nuttx-test-helper/src/main.rs
@@ -1,25 +1,23 @@
 use std::env;
 use std::fs;
-use std::io;
 use std::process::Command;
 
-fn main() -> io::Result<()> {
+fn main() {
     let mut command = Command::new(env::args().nth(1).expect("expects an argument"));
-    let args = fs::read_to_string("/etc/args.txt")?;
+    let args = fs::read_to_string("/etc/args.txt").expect("could not read /etc/args.txt");
     for line in args.lines() {
         command.arg(line);
     }
-    let env = fs::read_to_string("/etc/env.txt")?;
+    let env = fs::read_to_string("/etc/env.txt").expect("could not read /etc/env.txt");
     for line in env.lines() {
         if let Some((key, val)) = line.split_once('=') {
             command.env(key, val);
         }
     }
-    let output = command.output()?;
-    fs::write("/tmp/stdout.txt", output.stdout)?;
-    fs::write("/tmp/stderr.txt", output.stderr)?;
+    let output = command.output().expect("could not execute spawnee");
+    fs::write("/tmp/stdout.txt", output.stdout).expect("could not write to /tmp/stdout.txt");
+    fs::write("/tmp/stderr.txt", output.stderr).expect("could not write to /tmp/stderr.txt");
     let status = output.status.code().expect("expects exit status code");
-    fs::write("/tmp/exit-code.txt", status.to_string())?;
-
-    Ok(())
+    fs::write("/tmp/exit-code.txt", status.to_string())
+        .expect("could not write to /tmp/exit-code.txt");
 }

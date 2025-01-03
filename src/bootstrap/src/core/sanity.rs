a6cab926af18fcfb362cd6257d3881de97ad0a71
Pietro Albini <pietro.albini@ferrous-systems.com>
2025-01-03 10:28:22 +0100

    prevent bootstrap error when building the armv8r-nuttx-eabihf target

--- src/bootstrap/src/core/sanity.rs
+++ src/bootstrap/src/core/sanity.rs
@@ -37,6 +37,7 @@ pub struct Finder {
 const STAGE0_MISSING_TARGETS: &[&str] = &[
     // just a dummy comment so the list doesn't get onelined
     "aarch64-unknown-ferrocenecoretest",
+    "armv8r-nuttx-eabihf",
     "armv7-rtems-eabihf",
 ];
 

9e236607282f9769799c3f73cddd1356a9fb8b53
bdbai <bdbaiapp@163.com>
2025-01-06 16:46:24 +0800

    Move win arm shim to a separate file

--- src/windows_sys.rs
+++ src/windows_sys.rs
@@ -246,64 +246,6 @@ pub struct CONTEXT {
     pub ExtendedRegisters: [u8; 512],
 }
 
-cfg_if::cfg_if! {
-    if #[cfg(target_arch = "arm")] {
-        pub const ARM_MAX_BREAKPOINTS: usize = 8;
-        pub const ARM_MAX_WATCHPOINTS: usize = 1;
-
-        #[repr(C)]
-        #[derive(Clone, Copy)]
-        pub struct NEON128 {
-            pub Low: u64,
-            pub High: i64,
-        }
-
-        #[repr(C)]
-        #[derive(Clone, Copy)]
-        pub union CONTEXT_FloatRegs{
-            pub Q: [NEON128; 16],
-            pub D: [u64; 32],
-            pub S: [u32; 32],
-        }
-
-        #[repr(C)]
-        #[derive(Clone, Copy)]
-        pub struct CONTEXT {
-            pub ContextFlags: u32,
-            pub R0: u32,
-            pub R1: u32,
-            pub R2: u32,
-            pub R3: u32,
-            pub R4: u32,
-            pub R5: u32,
-            pub R6: u32,
-            pub R7: u32,
-            pub R8: u32,
-            pub R9: u32,
-            pub R10: u32,
-            pub R11: u32,
-            pub R12: u32,
-            // Control registers
-            pub Sp: u32,
-            pub Lr: u32,
-            pub Pc: u32,
-            pub Cpsr: u32,
-            // Floating-point registers
-            pub Fpsrc: u32,
-            pub Padding: u32,
-            pub u: CONTEXT_FloatRegs,
-            // Debug registers
-            pub Bvr: [u32; ARM_MAX_BREAKPOINTS],
-            pub Bcr: [u32; ARM_MAX_BREAKPOINTS],
-            pub Wvr: [u32; ARM_MAX_WATCHPOINTS],
-            pub Wcr: [u32; ARM_MAX_WATCHPOINTS],
-            pub Padding2: [u32; 2],
-        }
-
-        pub const IMAGE_FILE_MACHINE_ARMNT: IMAGE_FILE_MACHINE = 0x01c4;
-    }
-}
-
 pub type CONTEXT_FLAGS = u32;
 pub const CP_UTF8: u32 = 65001u32;
 pub type CREATE_TOOLHELP_SNAPSHOT_FLAGS = u32;
@@ -721,3 +663,6 @@ pub struct XSAVE_FORMAT {
     pub XmmRegisters: [M128A; 8],
     pub Reserved4: [u8; 224],
 }
+
+#[cfg(target_arch = "arm")]
+include!("./windows_sys_arm32_shim.rs");

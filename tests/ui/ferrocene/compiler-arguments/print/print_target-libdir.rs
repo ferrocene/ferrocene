a34f0642c8e9e0148077d849a93695229666909b
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2025-01-07 19:43:53 +0200

    update name of test directive
    See 835fbcbcab82ce728a7233de6c32a2d206e3336c

--- tests/ui/ferrocene/compiler-arguments/print/print_target-libdir.rs
+++ tests/ui/ferrocene/compiler-arguments/print/print_target-libdir.rs
@@ -1,16 +1,16 @@
 //@ check-pass
 //@ compile-flags: --print target-libdir
 //
-//@ normalize-stdout-test: "stage[012]" -> "$$STAGE"
-//@ normalize-stdout-test: "ci-rustc-sysroot" -> "$$STAGE"
+//@ normalize-stdout: "stage[012]" -> "$$STAGE"
+//@ normalize-stdout: "ci-rustc-sysroot" -> "$$STAGE"
 //
 // If this test fails on a new platform, add a new normalization annotation:
-//@ normalize-stdout-test: "x86_64-unknown-linux-gnu" -> "$$TARGET"
-//@ normalize-stdout-test: "x86_64-pc-nto-qnx710" -> "$$TARGET"
-//@ normalize-stdout-test: "aarch64-unknown-linux-gnu" -> "$$TARGET"
-//@ normalize-stdout-test: "aarch64-unknown-ferrocenecoretest" -> "$$TARGET"
-//@ normalize-stdout-test: "aarch64-apple-darwin" -> "$$TARGET"
-//@ normalize-stdout-test: "aarch64-unknown-nto-qnx710" -> "$$TARGET"
+//@ normalize-stdout: "x86_64-unknown-linux-gnu" -> "$$TARGET"
+//@ normalize-stdout: "x86_64-pc-nto-qnx710" -> "$$TARGET"
+//@ normalize-stdout: "aarch64-unknown-linux-gnu" -> "$$TARGET"
+//@ normalize-stdout: "aarch64-unknown-ferrocenecoretest" -> "$$TARGET"
+//@ normalize-stdout: "aarch64-apple-darwin" -> "$$TARGET"
+//@ normalize-stdout: "aarch64-unknown-nto-qnx710" -> "$$TARGET"
 
 fn main() {}
 

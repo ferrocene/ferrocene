5071877997607c22020374703123495b2bf8194c
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2025-01-10 15:23:18 +0200

    these tests are done elsewhere
    See parent commit

--- tests/ui/ferrocene/compiler-arguments/crate-type/crate-type_no-option.rs
+++
@@ -1,6 +0,0 @@
-//@ check-fail
-//@ compile-flags: --crate-type=
-
-fn main() {}
-
-// ferrocene-annotations: um_rustc_crate_type
--- tests/ui/ferrocene/compiler-arguments/crate-type/crate-type_staticlib.rs
+++
@@ -1,6 +0,0 @@
-//@ check-pass
-//@ compile-flags: --crate-type=staticlib
-
-pub fn foo() {}
-
-// ferrocene-annotations: um_rustc_crate_type

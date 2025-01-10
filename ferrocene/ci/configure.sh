aa261341705865eaf866b5a6bd3c7d42a6bc0168
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2025-01-10 13:39:47 +0100

    ferrocene/ci: configure cc

--- ferrocene/ci/configure.sh
+++ ferrocene/ci/configure.sh
@@ -188,6 +188,10 @@ if [[ is_internal ]]; then
     add --set target.x86_64-pc-nto-qnx710.cxx=q++
     add --set target.x86_64-pc-nto-qnx710.ar=ntox86_64-ar
     add --set target.x86_64-pc-nto-qnx710.profiler=false # Build failures were noted if this is enabled.
+
+    # these default to `cc` but require cross compilation
+    add --set target.thumbv7em-ferrocenecoretest-eabi.cc=arm-none-eabi-gcc
+    add --set target.thumbv7em-ferrocenecoretest-eabihf.cc=arm-none-eabi-gcc
 fi
 
 # Set the host platform to build. The environment variable is set from the CI

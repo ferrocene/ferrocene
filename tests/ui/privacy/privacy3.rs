c6cc91d3b607ca5f5ad50d4dba569df973ea993d
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2025-03-17 09:20:57 +0200

    fix upstream pull conflicts

--- tests/ui/privacy/privacy3.rs
+++ tests/ui/privacy/privacy3.rs
@@ -27,10 +27,6 @@ fn test1() {
 }
 
 fn main() {}
-<<<<<<< HEAD
-//~^ ERROR requires `sized` lang_item
 
 // ferrocene-annotations: fls_9gprp17h6t1q
 // Use Imports
-=======
->>>>>>> pull-upstream-temp--do-not-use-for-real-code

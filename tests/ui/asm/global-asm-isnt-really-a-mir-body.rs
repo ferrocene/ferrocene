7e172a554432f5d83ed4b6b1749945cfe38ea5da
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2025-03-25 17:08:52 +0200

    workaround linker fail

--- tests/ui/asm/global-asm-isnt-really-a-mir-body.rs
+++ tests/ui/asm/global-asm-isnt-really-a-mir-body.rs
@@ -24,3 +24,21 @@ fn foo() {}
 global_asm!("/* {} */", sym foo);
 
 fn main() {}
+
+// Ferrocene addition:
+//
+// Linker fails with "undefined symbol: __atomic_fetch_add_8" without this stub on these targets:
+// - thumbv7em-ferrocenecoretest-eabi
+// - thumbv7em-ferrocenecoretest-eabihf
+// This is due to `-C instrument-coverage` resulting in 64-bit atomic operations inserted,
+// but those do not exist in Cortex-M processors.
+//
+// Note that the code is not executed, so a stub is appropriate.
+#[cfg(target_arch = "arm")]
+#[cfg(not(target_has_atomic = "64"))]
+#[unsafe(no_mangle)]
+extern "C" fn __atomic_fetch_add_8() {
+    // This is added so we will get alerted if this test changes to being executed.
+    // The test is only compiled and linked at the moment.
+    unreachable!();
+}

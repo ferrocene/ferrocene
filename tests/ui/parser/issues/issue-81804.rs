5ae0f1aa8d434c2bda782ecc586d6371e162b2b6
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2025-04-04 06:29:13 +0200

    these tests fail when annotations are at bottom

--- tests/ui/parser/issues/issue-81804.rs
+++ tests/ui/parser/issues/issue-81804.rs
@@ -1,8 +1,8 @@
+// ferrocene-annotations: fls_qcb1n9c0e5hz
+// Functions
+
 fn main() {}
 
 //~vv ERROR mismatched closing delimiter: `}`
 //~v ERROR this file contains an unclosed delimiter
 fn p([=(}
-
-// ferrocene-annotations: fls_qcb1n9c0e5hz
-// Functions
--- tests/ui/parser/issues/issue-81827.rs
+++ tests/ui/parser/issues/issue-81827.rs
@@ -1,3 +1,6 @@
+// ferrocene-annotations: fls_qcb1n9c0e5hz
+// Functions
+
 #![crate_name="0"]
 
 fn main() {}
@@ -5,6 +8,3 @@ fn main() {}
 //~vv ERROR mismatched closing delimiter: `]`
 //~v ERROR this file contains an unclosed delimiter
 fn r()->i{0|{#[cfg(r(0{]0
-
-// ferrocene-annotations: fls_qcb1n9c0e5hz
-// Functions

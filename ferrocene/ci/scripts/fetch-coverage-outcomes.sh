8cde171036be809681e4125ac39da4a05d3c58b2
Pietro Albini <pietro.albini@ferrous-systems.com>
2025-04-04 13:42:14 +0200

    hook up coverage in CI

---
+++ ferrocene/ci/scripts/fetch-coverage-outcomes.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# SPDX-License-Identifier: MIT OR Apache-2.0
+# SPDX-FileCopyrightText: The Ferrocene Developers
+#
+# This script downloads all the build metrics for test jobs into the local
+# filesystem, filtering for builders with "test" in the name.
+
+set -euo pipefail
+IFS=$'\n\t'
+
+ARTIFACTS_BUCKET="ferrocene-ci-artifacts"
+ARTIFACTS_PREFIX="ferrocene/dist/"
+DEST="/tmp/coverage-outcomes"
+
+aws s3 cp --recursive "s3://${ARTIFACTS_BUCKET}/${ARTIFACTS_PREFIX}${CIRCLE_SHA1}/coverage/" "${DEST}"

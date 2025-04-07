4391e79da3e52665d9a4c5c8fb4cb9d26df42f92
Ana Hobden <operator@hoverbear.org>
2025-04-07 13:10:10 -0700

    Run qnx tests

--- ferrocene/ci/scripts/upload-dist-artifacts.sh
+++ ferrocene/ci/scripts/upload-dist-artifacts.sh
@@ -17,9 +17,7 @@ mkdir -p "${DIST_DIR}"
 
 if [[ -f "${BUILD_METRICS_FILE}" ]]; then
     mkdir -p "${DIST_DIR}/build-metrics"
-    DEST="${DIST_DIR}/build-metrics/${CIRCLE_JOB}.json"
-    cp "${BUILD_METRICS_FILE}" "$DEST"
-    echo "::notice title=Uploaded: $file::$DEST"
+    cp "${BUILD_METRICS_FILE}" "${DIST_DIR}/build-metrics/${CIRCLE_JOB}.json"
 fi
 
 if [[ -d "${COVERAGE_DIR}" ]]; then

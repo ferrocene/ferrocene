423fe5c30dfda256ea3ee1640087be0ae24374d4
Pietro Albini <pietro.albini@ferrous-systems.com>
2025-04-08 15:33:06 +0200

    ensure //@compile-flags are last even when passing --edition

--- src/tools/compiletest/src/header.rs
+++ src/tools/compiletest/src/header.rs
@@ -395,7 +395,9 @@ impl TestProps {
                     }
 
                     if let Some(edition) = config.parse_edition(ln) {
-                        self.compile_flags.push(format!("--edition={}", edition.trim()));
+                        // The edition is added at the start, since flags from //@compile-flags must
+                        // be passed to rustc last.
+                        self.compile_flags.insert(0, format!("--edition={}", edition.trim()));
                         has_edition = true;
                     }
 
@@ -627,7 +629,9 @@ impl TestProps {
         }
 
         if let (Some(edition), false) = (&config.edition, has_edition) {
-            self.compile_flags.push(format!("--edition={}", edition));
+            // The edition is added at the start, since flags from //@compile-flags must be passed
+            // to rustc last.
+            self.compile_flags.insert(0, format!("--edition={}", edition));
         }
     }
 

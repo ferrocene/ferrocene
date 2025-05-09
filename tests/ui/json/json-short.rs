7f93735b791c8afbe7a7777a952d74da2447e823
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2025-05-09 15:41:39 +0200

    ui/json-short: change span
    the error is always at the end of the file and the ferrocene version has
    extra `// ferrocene-annotations` comments so the span needs to be
    adjusted

--- tests/ui/json/json-short.rs
+++ tests/ui/json/json-short.rs
@@ -1,4 +1,4 @@
-//~v ERROR `main` function not found in crate `json_short`
+//~vvvv ERROR `main` function not found in crate `json_short`
 //@ compile-flags: --json=diagnostic-short --error-format=json
 
 // ferrocene-annotations: um_rustc_json

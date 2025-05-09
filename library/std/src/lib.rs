9fc2e6f66cc5d2241f73b5c5907644bfde8203b4
Johann Hemmann <johann.hemmann@ferrous-systems.com>
2025-05-09 15:23:13 +0200

    Disable coverage for code from libstd

--- library/std/src/lib.rs
+++ library/std/src/lib.rs
@@ -287,6 +287,7 @@
 #![feature(cfi_encoding)]
 #![feature(char_max_len)]
 #![feature(concat_idents)]
+#![feature(coverage_attribute)]
 #![feature(decl_macro)]
 #![feature(deprecated_suggestion)]
 #![feature(doc_cfg)]

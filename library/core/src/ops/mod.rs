b9bc9cca3c81f79cc788f0b0e2270fb1cdb8d222
Johann Hemmann <johann.hemmann@ferrous-systems.com>
2025-05-14 12:04:34 +0200

    ops::Receiver

--- library/core/src/ops/mod.rs
+++ library/core/src/ops/mod.rs
@@ -170,8 +170,8 @@ mod unsize;
 pub use self::deref::DerefPure;
 #[unstable(feature = "legacy_receiver_trait", issue = "none")]
 pub use self::deref::LegacyReceiver;
-// #[unstable(feature = "arbitrary_self_types", issue = "44874")]
-// pub use self::deref::Receiver;
+#[unstable(feature = "arbitrary_self_types", issue = "44874")]
+pub use self::deref::Receiver;
 #[stable(feature = "rust1", since = "1.0.0")]
 pub use self::deref::{Deref, DerefMut};
 #[stable(feature = "rust1", since = "1.0.0")]

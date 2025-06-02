b1d0cef9108e78ee2404968d557abdc169ac21ec
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2025-06-02 17:24:00 +0200

    adjust to new upstream build warning

--- tests/ui/ferrocene/compiler-arguments/print/print_native-static-libs.rs
+++ tests/ui/ferrocene/compiler-arguments/print/print_native-static-libs.rs
@@ -1,6 +1,7 @@
 //@ check-pass
 //@ compile-flags: --print native-static-libs
 //@ only-x86_64-unknown-linux-gnu
+//~? WARN cannot output linkage information without staticlib crate-type
 
 fn main() {}
 

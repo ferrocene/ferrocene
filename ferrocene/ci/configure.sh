83798693d3242f28601a6506e211d0afbf3951d8
Ana Hobden <operator@hoverbear.org>
2025-07-30 13:11:47 -0700

    Set musl-root

--- ferrocene/ci/configure.sh
+++ ferrocene/ci/configure.sh
@@ -125,6 +125,12 @@ add --set target."aarch64-unknown-ferrocene\.certified".cc=aarch64-linux-gnu-gcc
 add --set target."thumbv7em-ferrocene\.certified-eabi".cc=arm-none-eabi-gcc
 add --set target."thumbv7em-ferrocene\.certified-eabihf".cc=arm-none-eabi-gcc
 
+# musl toolchains use the architecture, also we need to set the `musl-root`
+add --set target.x86_64-unknown-linux-musl.musl-root=/usr/local/x86_64-linux-musl/
+add --set target.x86_64-unknown-linux-musl.cc=x86_64-linux-musl-gcc
+add --set target.aarch64-unknown-linux-musl.musl-root=/usr/local/aarch64-linux-musl/
+add --set target.aarch64-unknown-linux-musl.cc=aarch64-linux-musl-gcc
+
 # experiment to enable code coverage
 add --set 'target."aarch64-unknown-ferrocenecoretest".profiler=true'
 add --set 'target."thumbv7em-ferrocenecoretest-eabi".profiler=true'

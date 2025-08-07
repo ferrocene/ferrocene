a70bb34c8b77f7ee724b916752481f88fe2b8504
Johann Hemmann <johann.hemmann@ferrous-systems.com>
2025-08-07 11:00:53 +0200

    Add missing copyright headers in src/bootstrap/src/ferrocene

--- src/bootstrap/src/ferrocene/code_coverage.rs
+++ src/bootstrap/src/ferrocene/code_coverage.rs
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+// SPDX-FileCopyrightText: The Ferrocene Developers
+
 #[cfg(target_family = "unix")]
 use std::os::unix::fs::PermissionsExt;
 use std::path::PathBuf;

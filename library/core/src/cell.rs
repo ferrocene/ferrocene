c214c18f825b4d5a3adf60ebad30bd3c69fea9e0
Ana Hobden <operator@hoverbear.org>
2025-08-26 06:26:18 -0700

    Move annotations

--- library/core/src/cell.rs
+++ library/core/src/cell.rs
@@ -762,8 +762,8 @@ pub struct RefCell<T: ?Sized> {
 /// An error returned by [`RefCell::try_borrow`].
 #[stable(feature = "try_borrow", since = "1.13.0")]
 #[non_exhaustive]
-#[cfg(not(feature = "ferrocene_certified"))]
 #[derive(Debug)]
+#[cfg(not(feature = "ferrocene_certified"))]
 pub struct BorrowError {
     #[cfg(feature = "debug_refcell")]
     location: &'static crate::panic::Location<'static>,
@@ -790,8 +790,8 @@ impl Display for BorrowError {
 /// An error returned by [`RefCell::try_borrow_mut`].
 #[stable(feature = "try_borrow", since = "1.13.0")]
 #[non_exhaustive]
-#[cfg(not(feature = "ferrocene_certified"))]
 #[derive(Debug)]
+#[cfg(not(feature = "ferrocene_certified"))]
 pub struct BorrowMutError {
     #[cfg(feature = "debug_refcell")]
     location: &'static crate::panic::Location<'static>,

731a148b5e1a405fda05ecc9411aba7a4ba6e1f2
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2025-09-05 18:11:28 +0200

    build MUSL toolchains

---
+++ ferrocene/ci/docker-images/ubuntu-20/musl-toolchain.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+set -ex
+
+ARCH=$1
+TARGET=$ARCH-linux-musl
+OUTPUT=/usr/local
+MUSL_VER=1.2.5
+DL_CMD="curl -C - -L -o"
+
+git clone https://github.com/richfelker/musl-cross-make
+cd musl-cross-make
+git checkout fd6be58297ee21fcba89216ccd0d4aca1e3f1c5c # v0.9.11
+
+make -j$(nproc) TARGET=$TARGET MUSL_VER=$MUSL_VER DL_CMD="$DL_CMD"
+make install TARGET=$TARGET MUSL_VER=$MUSL_VER DL_CMD="$DL_CMD" OUTPUT=$OUTPUT
+
+cd -

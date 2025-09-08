dccc46057fc25faf357c956d37c0baac9e32c624
Jorge Aparicio <jorge.aparicio@ferrous-systems.com>
2025-09-08 14:37:24 +0200

    build MUSL as relocatable/PIC

--- ferrocene/ci/docker-images/ubuntu-20/musl-toolchain.sh
+++ ferrocene/ci/docker-images/ubuntu-20/musl-toolchain.sh
@@ -14,6 +14,10 @@ git clone https://github.com/richfelker/musl-cross-make
 cd musl-cross-make
 git checkout fd6be58297ee21fcba89216ccd0d4aca1e3f1c5c # v0.9.11
 
+# we need to build the code as relocatable / position independent (-fPIC) or else we get
+# linker errors with the x86_64 MUSL target. the `-g1` flag lets backtraces cross into C libraries
+export CFLAGS="-fPIC -g1 $CFLAGS"
+
 make -j$(nproc) TARGET=$TARGET MUSL_VER=$MUSL_VER DL_CMD="$DL_CMD"
 make install TARGET=$TARGET MUSL_VER=$MUSL_VER DL_CMD="$DL_CMD" OUTPUT=$OUTPUT
 

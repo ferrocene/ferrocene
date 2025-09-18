0c5e3c0a10d96caffd4493df48ed5c35de2f23b2
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2025-09-18 22:02:49 +0200

    minimum llvm bumped upstream

--- ferrocene/ci/scripts/setup-ubuntu.sh
+++ ferrocene/ci/scripts/setup-ubuntu.sh
@@ -12,8 +12,8 @@ sudo apt install -y \
 
 if [[ ! -z "${INSTALL_LLVM}" ]]; then
     sudo apt install -y \
-        llvm-19-tools \
-        llvm-19-dev
+        llvm-20-tools \
+        llvm-20-dev
 else
     echo 'Not installing LLVM, $INSTALL_LLVM is unset.'
 fi

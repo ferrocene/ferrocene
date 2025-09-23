0f27638ff6aa04dadfa26e6c2b28c77f65a975c8
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2025-09-23 07:48:41 +0200

    fix upstream conflicts

--- src/bootstrap/src/core/sanity.rs
+++ src/bootstrap/src/core/sanity.rs
@@ -38,7 +38,7 @@ const STAGE0_MISSING_TARGETS: &[&str] = &[
     // just a dummy comment so the list doesn't get onelined
     "aarch64_be-unknown-hermit",
     "aarch64_be-unknown-none-softfloat",
-<<<<<<< HEAD
+    "x86_64-unknown-motor",
     // Ferrocene additions
     "aarch64-unknown-ferrocene.facade",
     "thumbv7em-ferrocene.facade-eabi",
@@ -47,9 +47,6 @@ const STAGE0_MISSING_TARGETS: &[&str] = &[
     "x86_64-unknown-ferrocene.certified",
     "thumbv7em-ferrocene.certified-eabi",
     "thumbv7em-ferrocene.certified-eabihf",
-=======
-    "x86_64-unknown-motor",
->>>>>>> pull-upstream-temp--do-not-use-for-real-code
 ];
 
 /// Minimum version threshold for libstdc++ required when using prebuilt LLVM

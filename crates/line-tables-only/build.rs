1cea408bb02288d8611f7b83e8bc154cbaedd47e
Jubilee Young <workingjubilee@gmail.com>
2025-09-26 09:34:56 -0700

    comment on why not using -gline-tables-only

--- crates/line-tables-only/build.rs
+++ crates/line-tables-only/build.rs
@@ -3,6 +3,7 @@ fn main() {
 
     cc::Build::new()
         .opt_level(0)
+        // Don't use -gline-tables-only: it breaks on platforms that don't use clang (Linux with gcc, etc.)
         .flag("-g1")
         .file("src/callback.c")
         .compile("libcallback.a");

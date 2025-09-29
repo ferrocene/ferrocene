d8e3e81e0f391724de39ccc689890a8d6696cf79
Jynn Nelson <jynn.nelson@ferrous-systems.com>
2025-09-29 14:16:40 -0700

    fix some warnings

--- src/html_report.rs
+++ src/html_report.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashSet, fs, path::Path};
+use std::path::Path;
 
-use maud::{DOCTYPE, Markup, PreEscaped, Render};
+use maud::{DOCTYPE, PreEscaped};
 
 use crate::{CoverageStatus, FunctionCoverage};
 

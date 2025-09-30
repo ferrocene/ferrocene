45ff93fb9a69d46e091545b8c65f7a76e7948595
Johann Hemmann <johann.hemmann@ferrous-systems.com>
2025-09-30 15:34:36 +0200

    time::Duration

--- library/core/src/lib.rs
+++ library/core/src/lib.rs
@@ -368,7 +368,6 @@ pub mod hash;
 pub mod slice;
 #[cfg(not(feature = "ferrocene_certified"))]
 pub mod str;
-#[cfg(not(feature = "ferrocene_certified"))]
 pub mod time;
 
 #[cfg(not(feature = "ferrocene_certified"))]

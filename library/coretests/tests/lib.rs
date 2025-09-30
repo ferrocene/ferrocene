684a09d2a5a012beb01fbc89208ac5f3ffe8c753
Christian Poveda <christian.poveda@ferrous-systems.com>
2025-09-30 12:42:00 -0500

    Cover the `ptr_guaranteed_cmp` intrinsic

--- library/coretests/tests/lib.rs
+++ library/coretests/tests/lib.rs
@@ -23,6 +23,7 @@
 #![feature(const_mul_add)]
 #![feature(const_ops)]
 #![feature(const_option_ops)]
+#![feature(const_raw_ptr_comparison)]
 #![feature(const_ref_cell)]
 #![feature(const_result_trait_fn)]
 #![feature(const_trait_impl)]

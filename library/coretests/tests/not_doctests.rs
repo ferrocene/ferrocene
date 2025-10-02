bc7396ab4d3ab188483e7923635d22ba125fcb56
Christian Poveda <christian.poveda@ferrous-systems.com>
2025-10-02 18:06:16 -0500

    Test assorted `Result` methods

--- library/coretests/tests/not_doctests.rs
+++ library/coretests/tests/not_doctests.rs
@@ -98,3 +98,54 @@ fn option_methods() {
     assert_eq!(Some(None::<i32>).flatten(), None);
     assert_eq!(None::<Option<i32>>.flatten(), None);
 }
+
+#[test]
+fn result_methods() {
+    let s = String::from("hello");
+    let mut ok = Ok::<&&String, &&String>(&&s);
+    let mut err = Err::<&&String, &&String>(&&s);
+
+    let mut len = None;
+    err = err.inspect(|s| {
+        len = Some(s.len());
+    });
+    assert!(len.is_none());
+    assert!(err.is_err_and(|&x| x == &s));
+    assert!(!err.is_ok_and(|&x| x == &s));
+
+    ok = ok.inspect(|s| {
+        len = Some(s.len());
+    });
+    assert_eq!(len, Some(5));
+    assert!(ok.is_ok_and(|&x| x == &s));
+    assert!(!ok.is_err_and(|&x| x == &s));
+
+    let mut len = None;
+    ok = ok.inspect_err(|s| {
+        len = Some(s.len());
+    });
+    assert!(len.is_none());
+    assert!(ok.is_ok_and(|&x| x == &s));
+    assert!(!ok.is_err_and(|&x| x == &s));
+
+    err = err.inspect_err(|s| {
+        len = Some(s.len());
+    });
+    assert_eq!(len, Some(5));
+    assert!(err.is_err_and(|&x| x == &s));
+    assert!(!err.is_ok_and(|&x| x == &s));
+
+    assert_eq!(ok.copied().cloned().map_or_default(|s| s.len()), 5);
+    assert_eq!(err.copied().cloned().map_or_default(|s| s.len()), 0);
+
+    assert_eq!(ok.copied().cloned().map_or_else(|_| 0, |s| s.len()), 5);
+    assert_eq!(err.copied().cloned().map_or_else(|_| 0, |s| s.len()), 0);
+
+    let mut x = 5i32;
+
+    assert!(Ok::<&mut i32, i32>(&mut x).cloned().is_ok_and(|x| x == 5));
+    assert!(Ok::<&mut i32, i32>(&mut x).copied().is_ok_and(|x| x == 5));
+
+    assert!(Err::<&mut i32, i32>(x).cloned().is_err_and(|x| x == 5));
+    assert!(Err::<&mut i32, i32>(x).copied().is_err_and(|x| x == 5));
+}

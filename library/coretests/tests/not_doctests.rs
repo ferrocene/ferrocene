e3be3790076c6022c0d4fa079293d888e9ac3bd5
Christian Poveda <christian.poveda@ferrous-systems.com>
2025-10-02 17:42:00 -0500

    Test assorted `Option` methods

--- library/coretests/tests/not_doctests.rs
+++ library/coretests/tests/not_doctests.rs
@@ -48,3 +48,53 @@ fn bound_methods() {
     let unbounded = Bound::<&i32>::Unbounded.copied().map(|x| x + 1);
     assert_eq!(unbounded, Bound::Unbounded);
 }
+
+#[test]
+fn option_methods() {
+    let s = String::from("hello");
+    let mut some = Some(&&s);
+    let mut none = None::<&&String>;
+
+    let mut len = None;
+
+    none = none.inspect(|s| {
+        len = Some(s.len());
+    });
+    assert!(len.is_none());
+    assert!(none.is_none());
+
+    some = some.inspect(|s| {
+        len = Some(s.len());
+    });
+    assert_eq!(len, Some(5));
+    assert!(some.is_some_and(|&x| x == &s));
+
+    assert_eq!(some.copied().cloned().map_or_default(|s| s.len()), 5);
+    assert_eq!(none.copied().cloned().map_or_default(|s| s.len()), 0);
+
+    assert_eq!(some.reduce(some, |x, _| x), some);
+    assert_eq!(some.reduce(none, |x, _| x), some);
+    assert_eq!(none.reduce(some, |x, _| x), some);
+    assert_eq!(none.reduce(none, |x, _| x), none);
+
+    assert_eq!(some.xor(some), none);
+    assert_eq!(some.xor(none), some);
+    assert_eq!(none.xor(some), some);
+    assert_eq!(none.xor(none), none);
+
+    assert!(some.filter(|s| s.len() == 5).is_some_and(|&x| x == &s));
+    assert!(some.filter(|s| s.len() == 4).is_none());
+    assert!(none.filter(|_| true).is_none());
+
+    let mut x = 5i32;
+
+    assert!(Some(&mut x).cloned().is_some_and(|x| x == 5));
+    assert!(Some(&mut x).copied().is_some_and(|x| x == 5));
+
+    assert!(None::<&mut i32>.cloned().is_none());
+    assert!(None::<&mut i32>.copied().is_none());
+
+    assert_eq!(Some(Some(0i32)).flatten(), Some(0));
+    assert_eq!(Some(None::<i32>).flatten(), None);
+    assert_eq!(None::<Option<i32>>.flatten(), None);
+}

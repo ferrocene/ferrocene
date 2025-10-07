ec93be2919e033738255b95230076b0c2428586e
Ana Hobden <operator@hoverbear.org>
2025-10-07 05:58:04 -0700

    Migrate annotation

--- tests/ui/directory_ownership/file-mod-restriction.rs
+++ tests/ui/directory_ownership/file-mod-restriction.rs
@@ -3,3 +3,6 @@
 fn main() {
     mod foo; //~ ERROR cannot declare a file module inside a block unless it has a path attribute
 }
+
+// ferrocene-annotations: fls_e9hwvqsib5d5
+// Modules
--- tests/ui/directory_ownership/non-inline-mod-restriction.rs
+++
@@ -1,9 +0,0 @@
-<<<PULL-UPSTREAM>>> file deleted upstream; move the Ferrocene annotations if any, and delete this file
-// Test that non-inline modules are not allowed inside blocks.
-
-fn main() {
-    mod foo; //~ ERROR cannot declare a non-inline module inside a block
-}
-
-// ferrocene-annotations: fls_e9hwvqsib5d5
-// Modules

e5ab80dbcd5d7bf52998a6dd1a1ec840ae294974
Christian Poveda <christian.poveda@ferrous-systems.com>
2025-10-14 12:43:58 -0500

    Cover `type_name_of_val`

--- library/coretests/tests/not_doctests.rs
+++ library/coretests/tests/not_doctests.rs
@@ -419,3 +419,8 @@ fn unit_comparisons() {
     assert!(!(&mut () != &()));
     assert!(!(() < ()));
 }
+
+#[test]
+fn type_name() {
+    assert_eq!(core::any::type_name::<u8>(), core::any::type_name_of_val(&0u8));
+}

51489e22be5f13862bb8c495a21b1076cee4f82c
Christian Poveda <christian.poveda@ferrous-systems.com>
2025-10-14 14:50:05 -0500

    Cover `assume`

--- library/coretests/tests/not_doctests.rs
+++ library/coretests/tests/not_doctests.rs
@@ -424,3 +424,8 @@ fn unit_comparisons() {
 fn type_name() {
     assert_eq!(core::any::type_name::<u8>(), core::any::type_name_of_val(&0u8));
 }
+
+#[test]
+fn assume() {
+    unsafe { core::hint::assert_unchecked(true) };
+}

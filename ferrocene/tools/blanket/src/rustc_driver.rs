9494191d40ffd34e16de39fc3e506ea43f0db988
Urhengulas <johann.hemmann@ferrous-systems.com>
2025-10-22 20:46:42 +0200

    Have a single definition of SymbolReport and Function

--- ferrocene/tools/blanket/src/rustc_driver.rs
+++ ferrocene/tools/blanket/src/rustc_driver.rs
@@ -1,20 +1,10 @@
-use std::collections::{BTreeMap, BTreeSet};
 use std::fs::File;
-use std::path::PathBuf;
 
 use anyhow::{Context as _, Result};
+use build_helper::symbol_report::{Function, SymbolReport};
 use llvm_profparser::CoverageReport;
 
 use crate::{FunctionCoverage, ShowCommand, Span};
-
-#[derive(serde::Deserialize)]
-struct Function(String, PathBuf, usize, usize);
-
-#[derive(serde::Deserialize)]
-struct SymbolReport {
-    symbols: Vec<Function>,
-    annotations: BTreeMap<PathBuf, BTreeSet<(usize, usize)>>,
-}
 
 pub fn coverage(cmd: &ShowCommand, report: &CoverageReport) -> Result<Vec<FunctionCoverage>> {
     let symbol_report: SymbolReport = serde_json::from_reader(
@@ -22,9 +12,9 @@ pub fn coverage(cmd: &ShowCommand, report: &CoverageReport) -> Result<Vec<Functi
             .context(format!("failed to open symbol file {}", cmd.symbol_report.display()))?,
     )?;
     let mut coverage = vec![];
-    for Function(qualified_name, filename, start_line, end_line) in symbol_report.symbols {
+    for Function { qualified_name, filename, start_line, end_line } in symbol_report.symbols {
         let annotations = symbol_report.annotations.get(&filename);
-        let span = Span { filename, start_line, end_line };
+        let span = Span { filename: filename.into(), start_line, end_line };
         coverage.push(super::get_coverage(
             report,
             span,

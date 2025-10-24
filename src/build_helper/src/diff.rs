d12cb405d7ac408e18ba67e5c9b55afa9356e380
Urhengulas <johann.hemmann@ferrous-systems.com>
2025-10-24 20:07:01 +0200

    Move fn diff_text to build_helper

---
+++ src/build_helper/src/diff.rs
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+// SPDX-FileCopyrightText: The Ferrocene Developers
+
+use similar::{ChangeTag, TextDiff};
+
+/// Print a diff `expected` and `actual`
+pub fn diff_text(expected: &str, actual: &str) {
+    for change in TextDiff::from_lines(expected, actual).iter_all_changes() {
+        let sign = match change.tag() {
+            ChangeTag::Delete => "-",
+            ChangeTag::Insert => "+",
+            ChangeTag::Equal => " ",
+        };
+        print!("{}{}", sign, change);
+    }
+}

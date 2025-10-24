c02f287e8c521425c3734ff89d2e9cb2c12b80de
Urhengulas <johann.hemmann@ferrous-systems.com>
2025-10-24 20:12:37 +0200

    Refactor diff_text

--- src/build_helper/src/diff.rs
+++ src/build_helper/src/diff.rs
@@ -7,9 +7,9 @@ use similar::{ChangeTag, TextDiff};
 pub fn diff_text(expected: &str, actual: &str) {
     for change in TextDiff::from_lines(expected, actual).iter_all_changes() {
         let sign = match change.tag() {
-            ChangeTag::Delete => "-",
-            ChangeTag::Insert => "+",
-            ChangeTag::Equal => " ",
+            ChangeTag::Delete => '-',
+            ChangeTag::Insert => '+',
+            ChangeTag::Equal => ' ',
         };
         print!("{}{}", sign, change);
     }

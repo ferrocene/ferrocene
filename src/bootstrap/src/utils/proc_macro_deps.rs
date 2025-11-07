39ed2cae7260449dceb8495c86c7542a541e9a9d
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2025-11-07 09:38:52 +0200

    match main lockfile with that of upstream

--- src/bootstrap/src/utils/proc_macro_deps.rs
+++ src/bootstrap/src/utils/proc_macro_deps.rs
@@ -3,6 +3,7 @@
 /// See <https://github.com/rust-lang/rust/issues/134863>
 pub static CRATES: &[&str] = &[
     // tidy-alphabetical-start
+    "allocator-api2",
     "annotate-snippets",
     "anstyle",
     "askama_parser",
@@ -21,6 +22,7 @@ pub static CRATES: &[&str] = &[
     "fluent-langneg",
     "fluent-syntax",
     "fnv",
+    "foldhash",
     "generic-array",
     "hashbrown",
     "heck",

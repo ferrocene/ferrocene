22a0926c60dbb32a2d557b7e90cb851fda3649f9
Urhengulas <johann.hemmann@ferrous-systems.com>
2025-11-10 16:02:06 +0100

    Clone, Default, PartialEq for ops::Range

--- library/core/src/ops/range.rs
+++ library/core/src/ops/range.rs
@@ -78,7 +78,8 @@ impl fmt::Debug for RangeFull {
 /// ```
 #[lang = "Range"]
 #[doc(alias = "..")]
-#[cfg_attr(not(feature = "ferrocene_certified"), derive(Clone, Default, PartialEq, Eq, Hash))] // not Copy -- see #27186
+#[cfg_attr(not(feature = "ferrocene_certified"), derive(Eq, Hash))]
+#[derive_const(Clone, Default, PartialEq)] // not Copy -- see #27186
 #[stable(feature = "rust1", since = "1.0.0")]
 pub struct Range<Idx> {
     /// The lower bound of the range (inclusive).

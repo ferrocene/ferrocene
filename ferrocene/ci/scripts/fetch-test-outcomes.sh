cb8ed827e85516ae42880e13e822fc092c0a768c
Ana Hobden <operator@hoverbear.org>
2025-11-26 11:01:23 -0800

    Better set coverage/test outcomes paths

--- ferrocene/ci/scripts/fetch-test-outcomes.sh
+++ ferrocene/ci/scripts/fetch-test-outcomes.sh
@@ -10,10 +10,15 @@ IFS=$'\n\t'
 
 ARTIFACTS_BUCKET="${ARTIFACTS_BUCKET:-ferrocene-ci-artifacts}"
 ARTIFACTS_PREFIX="${ARTIFACTS_PREFIX:-ferrocene/dist}"
+SRC="s3://${ARTIFACTS_BUCKET}/${ARTIFACTS_PREFIX}/${CIRCLE_SHA1}/build-metrics/"
 DEST="/tmp/test-outcomes"
 
-aws s3 cp --recursive "s3://${ARTIFACTS_BUCKET}/${ARTIFACTS_PREFIX}/${CIRCLE_SHA1}/build-metrics/" "${DEST}"
+echo "Getting test outcomes from ${SRC}"
+
+aws s3 cp --recursive "${SRC}" "${DEST}"
 
 # Delete metrics for non-test jobs, to avoid including an unpredictable set of
 # metrics into the build (since the completion time of jobs is not constant).
 find "${DEST}" -type f \! \( -name "*test*" -or -name "misc-checks.json" \) -delete
+
+echo "Got test outcomes to ${DEST}"

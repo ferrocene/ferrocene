d8010b0aa0c239b3539d4cfd71570297460d3143
Ana Hobden <operator@hoverbear.org>
2025-11-26 11:01:23 -0800

    Better set coverage/test outcomes paths

--- ferrocene/ci/scripts/fetch-coverage-outcomes.sh
+++ ferrocene/ci/scripts/fetch-coverage-outcomes.sh
@@ -10,6 +10,11 @@ IFS=$'\n\t'
 
 ARTIFACTS_BUCKET="${ARTIFACTS_BUCKET:-ferrocene-ci-artifacts}"
 ARTIFACTS_PREFIX="${ARTIFACTS_PREFIX:-ferrocene/dist/}"
+SRC="s3://${ARTIFACTS_BUCKET}/${ARTIFACTS_PREFIX}/${CIRCLE_SHA1}/coverage/"
 DEST="/tmp/coverage-outcomes"
 
-aws s3 cp --recursive "s3://${ARTIFACTS_BUCKET}/${ARTIFACTS_PREFIX}${CIRCLE_SHA1}/coverage/" "${DEST}"
+echo "Getting coverage outcomes from ${SRC}"
+
+aws s3 cp --recursive "${SRC}" "${DEST}"
+
+echo "Got coverage outcomes in ${DEST}"

37f86c797dbf3077ba655fd04a806b83b5ae2373
Ana Hobden <operator@hoverbear.org>
2025-11-28 11:11:48 -0800

    Revert src/bootstrap/src/core/build_steps/compile.rs change

--- src/bootstrap/src/core/build_steps/compile.rs
+++ src/bootstrap/src/core/build_steps/compile.rs
@@ -675,7 +675,7 @@ pub fn std_cargo(
 
     let mut features = String::new();
 
-    if builder.no_std(target).unwrap() {
+    if builder.no_std(target) == Some(true) {
         features += " compiler-builtins-mem";
         if !target.starts_with("bpf") {
             features.push_str(compiler_builtins_c_feature);

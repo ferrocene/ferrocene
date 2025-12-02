9d16ef21c557f5866df1464cff36c04ab32ed76a
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2025-12-02 09:11:06 +0200

    adjust to changed/deleted tests

--- tests/ui/issues/issue-51022.rs
+++
@@ -1,6 +0,0 @@
-<<<PULL-UPSTREAM>>> file deleted upstream; move the Ferrocene annotations if any, and delete this file
-fn main<'a>() { }
-    //~^ ERROR `main` function is not allowed to have generic parameters [E0131]
-
-// ferrocene-annotations: fls_8jb3sjqamdpu
-// Program Entry Point

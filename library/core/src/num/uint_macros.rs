799272e157555fcce99ecfa3dddb1b7a25057263
Urhengulas <johann.hemmann@ferrous-systems.com>
2025-12-09 10:13:12 +0100

    uint::leading_ones

--- library/core/src/num/uint_macros.rs
+++ library/core/src/num/uint_macros.rs
@@ -172,7 +172,6 @@ macro_rules! uint_impl {
         #[must_use = "this returns the result of the operation, \
                       without modifying the original"]
         #[inline(always)]
-        #[cfg(not(feature = "ferrocene_subset"))]
         pub const fn leading_ones(self) -> u32 {
             (!self).leading_zeros()
         }

a8c4bc7982d624db44f874aeeec5179d02d9c9d8
Urhengulas <johann.hemmann@ferrous-systems.com>
2025-12-09 17:28:34 +0100

    vst1{neon_type[1].no}

--- library/core/src/lib.rs
+++ library/core/src/lib.rs
@@ -136,7 +136,7 @@
 //
 // Language features:
 // not-alphabetical-start
-#![cfg_attr(not(feature = "ferrocene_subset"), feature(abi_unadjusted))]
+#![feature(abi_unadjusted)]
 #![feature(adt_const_params)]
 #![feature(allow_internal_unsafe)]
 #![feature(allow_internal_unstable)]
@@ -163,7 +163,7 @@
 #![feature(intra_doc_pointers)]
 #![feature(intrinsics)]
 #![feature(lang_items)]
-#![cfg_attr(not(feature = "ferrocene_subset"), feature(link_llvm_intrinsics))]
+#![feature(link_llvm_intrinsics)]
 #![feature(macro_metavar_expr)]
 #![cfg_attr(not(feature = "ferrocene_subset"), feature(macro_metavar_expr_concat))]
 #![feature(marker_trait_attr)]
@@ -181,7 +181,7 @@
 #![feature(rustc_allow_const_fn_unstable)]
 #![feature(rustc_attrs)]
 #![feature(rustdoc_internals)]
-#![cfg_attr(not(feature = "ferrocene_subset"), feature(simd_ffi))]
+#![feature(simd_ffi)]
 #![feature(staged_api)]
 #![feature(stmt_expr_attributes)]
 #![feature(strict_provenance_lints)]
@@ -189,7 +189,7 @@
 #![feature(transparent_unions)]
 #![feature(try_blocks)]
 #![feature(unboxed_closures)]
-#![cfg_attr(not(feature = "ferrocene_subset"), feature(unsized_fn_params))]
+#![feature(unsized_fn_params)]
 #![cfg_attr(not(feature = "ferrocene_subset"), feature(with_negative_coherence))]
 // not-alphabetical-end
 //

64a454028f4ec1d16ec6adfb04236650049b016c
Ana Hobden <operator@hoverbear.org>
2025-12-10 13:44:08 -0800

    Fully cover <A as core::iter::traits::iterator::SpecIterEq<B>>::spec_iter_eq

--- library/coretests/tests/not_doctests.rs
+++ library/coretests/tests/not_doctests.rs
@@ -776,3 +776,11 @@ fn skip_as_iter_try_fold() {
     let folded = vals.iter().skip(10).try_fold(0, |x, y| std::io::Result::Ok(x + y)).unwrap();
     assert_eq!(folded, 0);
 }
+
+// <A as core::iter::traits::iterator::SpecIterEq<B>>::spec_iter_eq
+#[test]
+fn spec_iter_eq() {
+    let inf_1 = 0..;
+    let inf_2 = 1..;
+    assert_eq!(inf_1.into_iter().eq(inf_2), false);
+}

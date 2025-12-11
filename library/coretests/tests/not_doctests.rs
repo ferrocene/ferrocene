e7c8715db57f2a086a8be204467e30431045d53f
Urhengulas <johann.hemmann@ferrous-systems.com>
2025-12-11 17:10:06 +0100

    Cover `str::{from_utf8,from_utf8_mut,from_utf8_unchecked,from_utf8_unchecked_mut}`

--- library/coretests/tests/not_doctests.rs
+++ library/coretests/tests/not_doctests.rs
@@ -538,3 +538,61 @@ fn refcell_take() {
     assert_eq!(x.take(), 5);
     assert_eq!(*x.get_mut(), 0);
 }
+
+#[test]
+fn str_from_utf8_ok() {
+    let sparkle_heart = vec![240, 159, 146, 150];
+    let sparkle_heart = str::from_utf8(&sparkle_heart);
+    assert_eq!(Ok("💖"), sparkle_heart);
+}
+
+#[test]
+fn str_from_utf8_err() {
+    let sparkle_heart_err = vec![0, 159, 146, 150];
+    let sparkle_heart_err = str::from_utf8(&sparkle_heart_err);
+    assert!(sparkle_heart_err.is_err());
+}
+
+#[test]
+fn str_from_utf8_mut_ok() {
+    let mut sparkle_heart = vec![240, 159, 146, 150];
+    let sparkle_heart = str::from_utf8_mut(&mut sparkle_heart);
+    assert_eq!(Ok("💖"), sparkle_heart.as_deref());
+}
+
+#[test]
+fn str_from_utf8_mut_err() {
+    let mut sparkle_heart_err = vec![0, 159, 146, 150];
+    let sparkle_heart_err = str::from_utf8_mut(&mut sparkle_heart_err);
+    assert!(sparkle_heart_err.is_err());
+}
+
+#[test]
+fn str_from_utf8_unchecked_ok() {
+    let sparkle_heart = vec![240, 159, 146, 150];
+    let sparkle_heart = unsafe { str::from_utf8_unchecked(&sparkle_heart) };
+    assert_eq!("💖", sparkle_heart);
+}
+
+#[test]
+#[should_panic]
+fn str_from_utf8_unchecked_err() {
+    let sparkle_heart_err = vec![0, 159, 146, 150];
+    let sparkle_heart_err = unsafe { str::from_utf8_unchecked(&sparkle_heart_err) };
+    assert_eq!("💖", sparkle_heart_err);
+}
+
+#[test]
+fn str_from_utf8_unchecked_mut_ok() {
+    let mut sparkle_heart = vec![240, 159, 146, 150];
+    let sparkle_heart = unsafe { str::from_utf8_unchecked_mut(&mut sparkle_heart) };
+    assert_eq!("💖", sparkle_heart);
+}
+
+#[test]
+#[should_panic]
+fn str_from_utf8_unchecked_mut_err() {
+    let mut sparkle_heart_err = vec![0, 159, 146, 150];
+    let sparkle_heart_err = unsafe { str::from_utf8_unchecked_mut(&mut sparkle_heart_err) };
+    assert_eq!("💖", sparkle_heart_err);
+}

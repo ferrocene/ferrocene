0f47c982123232d9cd9442284573ef5b3137fa1f
Urhengulas <johann.hemmann@ferrous-systems.com>
2025-12-12 18:29:03 +0100

    Enable `arm_target_feature`

--- library/core/src/lib.rs
+++ library/core/src/lib.rs
@@ -196,7 +196,7 @@
 // Target features:
 // not-alphabetical-start
 #![cfg_attr(not(feature = "ferrocene_subset"), feature(aarch64_unstable_target_feature))]
-#![cfg_attr(not(feature = "ferrocene_subset"), feature(arm_target_feature))]
+#![feature(arm_target_feature)]
 #![cfg_attr(not(feature = "ferrocene_subset"), feature(avx10_target_feature))]
 #![cfg_attr(not(feature = "ferrocene_subset"), feature(hexagon_target_feature))]
 #![cfg_attr(not(feature = "ferrocene_subset"), feature(loongarch_target_feature))]

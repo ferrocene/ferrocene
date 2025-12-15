36be2c94e3bcd35be7b3d9c430522b54e05ad763
Christian Poveda <christian.poveda@ferrous-systems.com>
2025-12-15 12:38:35 -0500

    Cover `ExactSizeIterator::is_empty`

--- library/coretests/tests/not_doctests.rs
+++ library/coretests/tests/not_doctests.rs
@@ -457,3 +457,18 @@ fn as_mut_array() {
     assert!(slice.as_mut_array::<2>().is_none());
     assert!(slice.as_mut_array::<1>().is_some());
 }
+
+#[test]
+fn exact_iterator_mut_ref_is_empty() {
+    assert!((&mut (0..0)).is_empty());
+    assert!(!(&mut (0..1)).is_empty());
+}
+
+#[test]
+fn chunks_exact_is_empty() {
+    assert!([0; 10].chunks_exact(11).is_empty());
+    assert!(![0; 10].chunks_exact(2).is_empty());
+
+    assert!([0; 10].chunks_exact_mut(11).is_empty());
+    assert!(![0; 10].chunks_exact_mut(2).is_empty());
+}

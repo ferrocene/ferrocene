4862644c659d2815d1fe0657de4cc0f29c713ba2
Christian Poveda <christian.poveda@ferrous-systems.com>
2025-12-18 15:32:14 -0500

    Cover `core::ptr`

--- library/coretests/tests/not_doctests.rs
+++ library/coretests/tests/not_doctests.rs
@@ -685,3 +685,63 @@ fn maybe_uninit() {
     assert_eq!(source, u64::MAX);
     assert_eq!(destination, u64::MAX);
 }
+
+// covers `core::ptr::const_ptr::<impl *const T>::align_offset`
+#[test]
+#[should_panic = "align_offset: align is not a power-of-two"]
+fn non_power_of_two_align_offset() {
+    let ptr: *const () = &();
+    let _ = ptr.align_offset(3);
+}
+
+// covers:
+// - `core::ptr::non_null::NonNull::<T>::sub`
+// - `core::ptr::mut_ptr::<impl *mut T>::sub`
+#[test]
+fn zst_sub_is_noop() {
+    let ptr = core::ptr::NonNull::from_ref(&());
+    // SAFETY: `ptr` is a pointer to a ZST so substracting anything from it is a noop.
+    assert_eq!(ptr, unsafe { ptr.sub(isize::MAX as usize) });
+    assert_eq!(ptr.as_ptr(), unsafe { ptr.as_ptr().sub(isize::MAX as usize) });
+}
+
+// covers:
+// - `core::ptr::const_ptr::<impl core::cmp::Ord for *const T>::cmp`
+// - `core::ptr::const_ptr::<impl core::cmp::PartialOrd for *const T>::ge`
+// - `core::ptr::const_ptr::<impl core::cmp::PartialOrd for *const T>::gt`
+// - `core::ptr::const_ptr::<impl core::cmp::PartialOrd for *const T>::le`
+// - `core::ptr::const_ptr::<impl core::cmp::PartialOrd for *const T>::lt`
+// - `core::ptr::const_ptr::<impl core::cmp::PartialOrd for *const T>::partial_cmp`
+#[test]
+fn ptr_partial_ord() {
+    let arr = [0, 1];
+
+    let fst = arr.as_ptr();
+    let snd = unsafe { fst.add(1) };
+
+    assert!(fst.le(&fst));
+    assert!(fst.le(&snd));
+    assert!(fst.lt(&snd));
+
+    assert!(snd.ge(&snd));
+    assert!(snd.ge(&fst));
+    assert!(snd.gt(&fst));
+
+    assert_eq!(fst.partial_cmp(&fst), Some(core::cmp::Ordering::Equal));
+    assert_eq!(fst.partial_cmp(&snd), Some(core::cmp::Ordering::Less));
+    assert_eq!(snd.partial_cmp(&fst), Some(core::cmp::Ordering::Greater));
+}
+
+// covers:
+// - `core::ptr::read_volatile`
+// - `core::ptr::write_volatile`
+#[test]
+fn volatile_ops() {
+    let mut x = 0;
+    let y = &mut x as *mut i32;
+
+    unsafe {
+        std::ptr::write_volatile(y, 12);
+        assert_eq!(std::ptr::read_volatile(y), 12);
+    }
+}

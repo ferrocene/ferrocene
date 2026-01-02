cfd8f35cb27c638ecd75ebf1c89f957f66b2ad9a
Ana Hobden <operator@hoverbear.org>
2026-01-02 10:01:37 -0800

    Fully cover <core::iter::adapters::copied::Copied<I> as core::iter::traits::iterator::Iterator>::last

--- library/coretests/tests/not_doctests/iter.rs
+++ library/coretests/tests/not_doctests/iter.rs
@@ -3,6 +3,14 @@
 fn test_iterator_copied_advance_by() {
     let first = vec![1, 2];
     let mut iter = first.iter().copied();
-    iter.advance_by(1).ok(); // Go past `self.a`
+    iter.advance_by(1).ok();
     assert_eq!(iter.next(), Some(2));
+}
+
+// <core::iter::adapters::copied::Copied<I> as core::iter::traits::iterator::Iterator>::last
+#[test]
+fn test_iterator_copied_last() {
+    let first = vec![1, 2];
+    let iter = first.iter().copied();
+    assert_eq!(iter.last(), Some(2));
 }

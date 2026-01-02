da7e7658b99e77bb346135814e8e694def9924f5
Ana Hobden <operator@hoverbear.org>
2026-01-02 08:58:42 -0800

    Fully cover core::time::Duration::from_secs_f32

--- library/coretests/tests/not_doctests/time.rs
+++ library/coretests/tests/not_doctests/time.rs
@@ -1,8 +1,17 @@
+use core::time::Duration;
+
 // core::time::Duration::checked_add
 #[test]
 fn duration_checked_add() {
     let one_billion = 1_000_000_000;
-    let max_duration = core::time::Duration::new(u64::MAX, one_billion - 1);
-    let more_duration = core::time::Duration::new(0, 2);
+    let max_duration = Duration::new(u64::MAX, one_billion - 1);
+    let more_duration = Duration::new(0, 2);
     assert_eq!(max_duration.checked_add(more_duration), None);
+}
+
+// core::time::Duration::from_secs_f32
+#[test]
+#[should_panic]
+fn duration_try_from_secs_f32() {
+    let _nope = Duration::from_secs_f32(-1.0);
 }

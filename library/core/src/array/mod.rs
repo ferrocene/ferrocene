4951bf29989d32194130756934782f07370fed78
Urhengulas <johann.hemmann@ferrous-systems.com>
2026-01-07 16:24:55 +0100

    Cover `<&mut core::array::drain::Drain<'_, '_, T, N, F> as core::ops::function::FnOnce<(usize,)>>::call_once`

--- library/core/src/array/mod.rs
+++ library/core/src/array/mod.rs
@@ -1041,3 +1041,19 @@ fn iter_next_chunk_erased<T>(
     mem::forget(guard);
     Ok(())
 }
+
+/// Ferrocene addition: Hidden module to test crate-internal functionality
+#[doc(hidden)]
+#[unstable(feature = "ferrocene_test", issue = "none")]
+#[cfg(not(feature = "ferrocene_subset"))]
+pub mod ferrocene_test {
+    use super::*;
+
+    pub fn test_drain_call_once() {
+        let mut md = ManuallyDrop::new([1, 2, 3, 4, 5]);
+        let mut f = |c| c + 10;
+        // SAFETY: is only called once
+        let mut drain = unsafe { drain::Drain::new(&mut md, &mut f) };
+        assert_eq!(11, drain.call_once((15,)));
+    }
+}
--- library/coretests/tests/not_doctests.rs
+++ library/coretests/tests/not_doctests.rs
@@ -1,3 +1,4 @@
+mod array;
 mod atomic;
 mod borrow;
 mod cell;

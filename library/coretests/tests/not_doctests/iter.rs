bdc0ea19b1fb6bfbc85df5f5527f606ba45d1829
Urhengulas <johann.hemmann@ferrous-systems.com>
2026-01-07 16:26:12 +0100

    Cover `Iterator` and `DoubleEndedIterator` for `IndexRange`

--- library/coretests/tests/not_doctests/iter.rs
+++ library/coretests/tests/not_doctests/iter.rs
@@ -399,3 +399,13 @@ fn test_iter_mut_as_mut_slice() {
     let mut iter = x.iter_mut();
     assert_eq!([1, 2, 3], iter.as_mut_slice());
 }
+
+#[test]
+fn test_index_range_double_ended_iterator() {
+    core::ferrocene_test::test_index_range_double_ended_iterator();
+}
+
+#[test]
+fn test_index_range_iterator() {
+    core::ferrocene_test::test_index_range_iterator();
+}

eb4f54cc5b1cffa534c70673d08c2c53dbc9a646
Jynn Nelson <jynn.nelson@ferrous-systems.com>
2026-01-13 14:24:07 -0500

    wip: Implement #[ferrocene::prevalidated]
    future work:
    - ferrocene::exported_constraint
    - ferrocene::validated
    - cfg-specific validations
    - hook this up to core so we can get rid of the subset targets
    
    implementation:
    - use HIR node of instantiation
    
        The actual call that errors might be in another crate, in which case we
        have no way to lint it. Lint the instantiation in the current crate
        instead.
    
    - i'm really really happy with these diagnostics
        - find appropriate impl for dyn Trait upcast
        - annotate drop_in_place to get better diagnostics
    
    - custom RootCollector
    - avoid linting on constants more than necessary
    
        otherwise we have to annotate half of core and it's really annoying
    
    - disallow casts to `dyn trait`
    
        this is too eager to lint -- it should only lint if one of the type's
        impls isn't marked with `ferrocene::prevalidated`. but finding the impl
        is hard and this check is easy.
    
    - checks
        - function pointers in consts
        - closures
        - unsizing casts
    
    - tests
        - dyn traits
        - lint levels
    
    - lots of docs

---
+++ compiler/rustc_lint/src/ferrocene/diagnostics.rs
@@ -0,0 +1,161 @@
+//! ## Recommended reading
+//! - [Errors and lints](https://rustc-dev-guide.rust-lang.org/diagnostics.html)
+
+use rustc_errors::{Diag, MultiSpan};
+use rustc_hir::HirId;
+use rustc_hir::def_id::DefId;
+use rustc_span::{STDLIB_STABLE_CRATES, Span};
+use tracing::debug;
+
+use crate::ferrocene::post_mono::InstantiationSite;
+use crate::ferrocene::{LintState, UNCERTIFIED, Use, UseKind};
+
+/// Diagnostics.
+impl<'tcx> LintState<'tcx> {
+    fn func_span(&self, def_id: DefId) -> Span {
+        match self.tcx.opt_item_ident(def_id) {
+            Some(name) => name.span,
+            None => self.tcx.def_span(def_id),
+        }
+    }
+
+    pub(super) fn lint_use(&mut self, lint_node: HirId, use_: Use<'tcx>) {
+        let Self { tcx, item: owner, .. } = *self;
+        let (callee, receiver_span) = (use_.def_id(), use_.span);
+
+        debug!("linting node {lint_node:?}");
+        tcx.node_span_lint(UNCERTIFIED, lint_node, receiver_span, |diag| {
+            let callee_descr = tcx.def_descr(callee);
+            let owner_descr = tcx.def_descr(owner.into());
+            diag.primary_message(format!(
+                "validated {owner_descr} {} an unvalidated {callee_descr}",
+                use_.present_tense()
+            ));
+
+            // Need to do this lazily or `with_no_trimmed_paths` will panic :/
+            let name = match use_.opt_instance() {
+                None => tcx.def_path_str(callee),
+                Some(instance) => tcx.def_path_str_with_args(callee, instance.args),
+            };
+            // TODO: this is horrible for `Cast` or `ContainsTy`. Need to rewrite all this
+            // logic to bring the appropriate data into `lint` and then actually look at
+            // the discriminant instead of just collapsing it to `name`.
+            diag.span_label(self.func_span(callee), format!("`{name}` is unvalidated"));
+
+            if let UseKind::ContainsFnPtr(_, ty) = use_.kind {
+                diag.note(format!("`{name}` contains a function pointer that might be called at runtime"));
+                diag.note(format!("the Ferrocene compiler does not know if the `{ty}` was verified, so it must assume it is unverified"));
+            }
+
+            if STDLIB_STABLE_CRATES.contains(&tcx.crate_name(callee.krate)) {
+                diag.help_once(format!(
+                    "contact Ferrocene support to see if this {callee_descr} is possible to certify"
+                ));
+            }
+
+            // Don't show this "takes place in a certified function" label more than once per function.
+            // We really do need this as a separate bit of state from shown_lints because the lint might not be
+            // emitted. ideally we would just `cancel` the diagnostic if we don't want to emit it,
+            // but we don't get an owned `Diag` from `node_span_lint` :(
+            if !self.shown_item {
+                self.shown_item = true;
+                let mut validated_span = MultiSpan::from_span(self.func_span(owner.into()));
+                if let Some(annotation) = self.annotation {
+                    validated_span.push_span_label(annotation, "marked as validated here");
+                }
+
+                self.decorate_cast(use_, diag);
+                self.decorate_instantiation(use_, diag, Some(&mut validated_span));
+
+                diag.span_note(
+                    validated_span,
+                    format!("`{}` is validated", tcx.def_path_str(owner)),
+                );
+                if self.annotation.is_none() {
+                    diag.note("main functions are assumed to be validated");
+                }
+            } else {
+                self.decorate_cast(use_, diag);
+                self.decorate_instantiation(use_, diag, None);
+            }
+        });
+    }
+
+    fn decorate_cast(&self, use_: Use<'tcx>, diag: &mut Diag<'_, ()>) {
+        let tcx = self.tcx;
+        if matches!(use_.kind, UseKind::FnPtrCast(..)) {
+            diag.note("once a function is cast to a function pointer, Ferrocene can no longer tell whether it is validated");
+            diag.note("as a precaution, it must assume you will eventually call the function");
+        } else if let UseKind::TraitObjectCast(assoc_fn, ty) = use_.kind {
+            if let Some(trait_) = tcx.trait_of_assoc(assoc_fn) {
+                diag.note(format!(
+                    "Ferrocene cannot tell whether `{ty}`'s implementation of `{}` is validated",
+                    tcx.def_path_str(trait_)
+                ));
+                diag.note(format!(
+                    "as a precaution, it must assume `{}` is unvalidated",
+                    tcx.def_path_str(assoc_fn)
+                ));
+            }
+            diag.note("once an object is cast to a dynamic trait object, Ferrocene can no longer tell whether it is validated");
+            diag.note(format!(
+                "as a precaution, it must assume you will eventually call `{}`",
+                tcx.def_path_str(assoc_fn)
+            ));
+        }
+    }
+
+    fn decorate_instantiation(
+        &self,
+        use_: Use<'tcx>,
+        diag: &mut Diag<'_, ()>,
+        validated_span: Option<&mut MultiSpan>,
+    ) {
+        let tcx = self.tcx;
+        if let Some(InstantiationSite {
+            caller_span,
+            caller_instance,
+            pre_mono_item,
+            lint_node: _,
+        }) = use_.from_instantiation
+        {
+            let callee_descr = format!(
+                "generic {} `{}`",
+                tcx.def_descr(pre_mono_item),
+                rustc_middle::ty::print::with_no_trimmed_paths!(tcx.def_path_str(pre_mono_item))
+            );
+
+            let msg = format!(
+                "{callee_descr} instantiated by `{}`",
+                tcx.def_path_str_with_args(caller_instance.def_id(), caller_instance.args)
+            );
+
+            if let Some(multi) = validated_span {
+                multi.push_span_label(caller_span, msg);
+            } else {
+                diag.span_note_once(
+                    caller_span,
+                    format!("{msg}, which is called from a validated entrypoint"),
+                );
+            }
+            // } else {
+            //     diag.note(format!(
+            //             "{callee_descr} instantiated by validated entrypoint {}",
+            //             tcx.def_path_str(self.item)
+            //     ));
+        }
+    }
+}
+
+impl<'tcx> Use<'tcx> {
+    pub(super) fn present_tense(self) -> &'static str {
+        match self.kind {
+            UseKind::Called(..) => "calls",
+            UseKind::Named(..) => "uses",
+            // originally this said "type-erases" but that's unfamiliar jargon, and it's not clear
+            // that it actually helps understanding.
+            UseKind::TraitObjectCast(..) | UseKind::FnPtrCast(..) => "possibly calls",
+            UseKind::ContainsFnPtr(..) => "uses",
+        }
+    }
+}
---
+++ tests/ui/ferrocene/lint-uncertified.rs
@@ -0,0 +1,170 @@
+// Don't use anything from core here.
+// If you do, it will break once we start annotating more of core.
+// You can still use traits as long as the impls use custom types.
+// In particular do NOT use numerics anywhere here,
+// nor any syntax in core::ops unless it's with an ADT.
+
+//@ revisions: dedup no-dedup post-mono
+//@[dedup] compile-flags: -Z deduplicate-diagnostics=yes
+//@[post-mono] compile-flags: -Z deduplicate-diagnostics=yes
+
+#![crate_type = "lib"]
+#![deny(ferrocene::uncertified)]
+
+use std::clone::Clone;
+use std::cmp::{PartialEq, PartialOrd};
+
+fn normal_def() {
+    normal_def2(); // ok
+}
+
+fn normal_def2() {}
+
+#[ferrocene::prevalidated]
+const fn marked_certified() {}
+
+const fn const_fn() {}
+
+extern "C" { fn extern_fn(); }
+
+macro_rules! mbe {
+    () => { normal_def() }
+    //[no-dedup]~^ ERROR unvalidated
+    //[no-dedup]~^^ ERROR unvalidated
+}
+
+struct Ctor;
+
+#[derive(Clone)]
+struct Unvalidated;
+#[derive(Clone)]
+struct Unvalidated2;
+
+#[ferrocene::prevalidated]
+#[derive(Clone)]
+struct Validated;
+
+impl Unvalidated {
+    fn inherent_fn(&self) {}
+}
+
+impl Copy for Unvalidated {}
+impl Copy for Validated {}
+
+impl PartialOrd for Unvalidated {
+    fn partial_cmp(&self, _: &Self) -> Option<std::cmp::Ordering> {
+        None
+    }
+}
+
+impl PartialEq<Unvalidated> for Unvalidated {
+    #[ferrocene::prevalidated]
+    fn eq(&self, _: &Self) -> bool {
+        true
+    }
+
+    fn ne(&self, _: &Self) -> bool {
+        false
+    }
+}
+
+impl ToString for Validated {
+    #[ferrocene::prevalidated]
+    fn to_string(&self) -> String {
+        String::new()
+    }
+}
+
+trait FancyClone {
+    fn fancy_clone();
+}
+impl<T: Clone> FancyClone for T {
+    fn fancy_clone() {}
+}
+
+const UNCERTIFIED_CLOSURE_CONST: fn() = || normal_def();
+static UNCERTIFIED_CLOSURE_STATIC: fn() = || normal_def();
+const UNCERTIFIED_DYN_CONST: &'static (dyn Sync + PartialOrd<Unvalidated>) = &Unvalidated;
+static UNCERTIFIED_DYN_STATIC: &'static (dyn Sync + PartialOrd<Unvalidated>) = &Unvalidated;
+
+#[ferrocene::prevalidated]
+const CERTIFIED_CLOSURE_CONST: fn() = || normal_def(); //~ ERROR unvalidated
+//[no-dedup]~^ ERROR unvalidated
+#[ferrocene::prevalidated]
+static CERTIFIED_CLOSURE_STATIC: fn() = || normal_def(); //~ ERROR unvalidated
+//[no-dedup]~^ ERROR unvalidated
+#[ferrocene::prevalidated]
+const CERTIFIED_DYN_CONST: &'static (dyn Sync + PartialOrd<Unvalidated>) = &Unvalidated;
+//~^ ERROR unvalidated
+#[ferrocene::prevalidated]
+static CERTIFIED_DYN_STATIC: &'static (dyn Sync + PartialOrd<Unvalidated>) = &Unvalidated;
+//~^ ERROR unvalidated
+
+#[cfg(post_mono)]
+#[ferrocene::prevalidated]
+pub fn reachable() {
+    certified();
+}
+
+#[ferrocene::prevalidated]
+fn certified() {
+    normal_def(); //~ ERROR unvalidated
+    //[no-dedup]~^ ERROR unvalidated
+    let fn_ptr: fn() = normal_def;
+    //[no-dedup]~^ ERROR unvalidated
+    fn_ptr(); // ok
+
+    let trait_fn_type = <Unvalidated as Clone>::clone; //~ ERROR unvalidated
+    trait_fn_type(&Unvalidated);
+    //[no-dedup]~^ ERROR unvalidated
+    let trait_fn_ptr: fn(&Unvalidated) -> Unvalidated = trait_fn_type;
+    trait_fn_ptr(&Unvalidated); // ok
+
+    let dyn_trait: &dyn PartialOrd<Unvalidated> = &Unvalidated; //~ ERROR unvalidated
+    dyn_trait.partial_cmp(&Unvalidated); // ok
+
+    let dyn_trait_partially_certified: &dyn PartialEq<Unvalidated> = &Unvalidated;
+    //~^ ERROR unvalidated
+    dyn_trait_partially_certified.eq(&Unvalidated); // ok
+    dyn_trait_partially_certified.ne(&Unvalidated); // ok
+
+    let dyn_trait_fully_certified: &dyn ToString = &Validated; // ok: this impl is validated
+    dyn_trait_fully_certified.to_string(); // ok
+
+    Ctor; // ok
+    mbe!(); // caught in macro definition above; maybe we should have both spans?
+    Unvalidated.inherent_fn(); //~ ERROR unvalidated
+    //[no-dedup]~^ ERROR unvalidated
+    Unvalidated.clone();
+    //[no-dedup]~^ ERROR unvalidated
+    //[no-dedup]~^^ ERROR unvalidated
+    marked_certified(); // ok
+
+    // TODO: test turbofish
+
+    UNCERTIFIED_CONST; // ok
+    UNCERTIFIED_CLOSURE_CONST(); //~ ERROR unvalidated
+    UNCERTIFIED_CLOSURE_STATIC(); //~ ERROR unvalidated
+    UNCERTIFIED_DYN_CONST.partial_cmp(&Unvalidated); //~ ERROR unvalidated
+    UNCERTIFIED_DYN_STATIC.partial_cmp(&Unvalidated); //~ ERROR unvalidated
+
+    CERTIFIED_CLOSURE_CONST(); // ok
+    CERTIFIED_CLOSURE_STATIC(); // ok
+    CERTIFIED_DYN_CONST.partial_cmp(&Unvalidated); // ok
+    CERTIFIED_DYN_STATIC.partial_cmp(&Unvalidated); // ok
+
+    use normal_def as rename;
+    rename();
+    //[no-dedup]~^ ERROR unvalidated
+    //[no-dedup]~^^ ERROR unvalidated
+
+    unsafe { extern_fn(); } // TODO this should lint
+    // TODO: specialization
+    // TODO: test trait defaults both ways
+    // TODO: panic hooks are likely broken
+}
+
+#[ferrocene::prevalidated]
+const CERTIFIED_CONST2: () = marked_certified(); // ok
+
+const UNCERTIFIED_CONST: () = const_fn(); // ok

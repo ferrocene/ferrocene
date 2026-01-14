47355f7b5f4cb370f26b20ffc937b13470b22a4e
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2026-01-14 20:26:18 +0200

    exclude new APIs from certified core subset

--- library/stdarch/crates/core_arch/src/simd.rs
+++ library/stdarch/crates/core_arch/src/simd.rs
@@ -2,6 +2,7 @@
 
 #![allow(non_camel_case_types)]
 
+#[cfg(not(feature = "ferrocene_subset"))]
 #[inline(always)]
 #[rustc_const_unstable(feature = "stdarch_const_helpers", issue = "none")]
 pub(crate) const unsafe fn simd_imax<T: Copy>(a: T, b: T) -> T {
@@ -9,6 +10,7 @@ pub(crate) const unsafe fn simd_imax<T: Copy>(a: T, b: T) -> T {
     crate::intrinsics::simd::simd_select(mask, a, b)
 }
 
+#[cfg(not(feature = "ferrocene_subset"))]
 #[inline(always)]
 #[rustc_const_unstable(feature = "stdarch_const_helpers", issue = "none")]
 pub(crate) const unsafe fn simd_imin<T: Copy>(a: T, b: T) -> T {

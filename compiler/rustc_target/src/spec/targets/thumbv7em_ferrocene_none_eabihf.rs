67706988cbe2864f86ffc04bb781b9e8654e3e72
Urhengulas <johann.hemmann@ferrous-systems.com>
2026-01-21 13:08:26 +0100

    Remove `-ferrocene-none` targets

--- compiler/rustc_target/src/spec/targets/thumbv7em_ferrocene_none_eabihf.rs
+++
@@ -1,7 +0,0 @@
-use crate::spec::Target;
-
-pub(crate) fn target() -> Target {
-    let target = super::thumbv7em_none_eabihf::target();
-
-    target
-}
--- compiler/rustc_target/src/spec/targets/x86_64_ferrocene_none.rs
+++
@@ -1,7 +0,0 @@
-use crate::spec::Target;
-
-pub(crate) fn target() -> Target {
-    let target = super::x86_64_unknown_none::target();
-
-    target
-}

dc3255978b473ac34b70f1d133d2c6ae9ebb8f2c
Urhengulas <johann.hemmann@ferrous-systems.com>
2026-01-29 21:19:28 +0100

    Cover `core::fmt::FormattingOptions::{align,width}`

--- library/coretests/tests/not_doctests/fmt.rs
+++ library/coretests/tests/not_doctests/fmt.rs
@@ -5,3 +5,42 @@ use core::fmt;
 fn test_rt_argument_as_u16_none() {
     fmt::ferrocene_test::test_rt_argument_as_u16_none();
 }
+
+// Covers `core::fmt::FormattingOptions::width`
+#[test]
+fn test_formatting_options_width() {
+    let mut a = fmt::FormattingOptions::new();
+    assert_eq!(a.width(None).get_width(), None);
+}
+
+// Covers `core::fmt::FormattingOptions::align`
+#[test]
+fn test_formatting_options_align_left() {
+    let mut fo = fmt::FormattingOptions::new();
+    let alignment = Some(fmt::Alignment::Left);
+    assert_eq!(fo.align(alignment).get_align(), alignment);
+}
+
+// Covers `core::fmt::FormattingOptions::align`
+#[test]
+fn test_formatting_options_align_right() {
+    let mut fo = fmt::FormattingOptions::new();
+    let alignment = Some(fmt::Alignment::Right);
+    assert_eq!(fo.align(alignment).get_align(), alignment);
+}
+
+// Covers `core::fmt::FormattingOptions::align`
+#[test]
+fn test_formatting_options_align_center() {
+    let mut fo = fmt::FormattingOptions::new();
+    let alignment = Some(fmt::Alignment::Center);
+    assert_eq!(fo.align(alignment).get_align(), alignment);
+}
+
+// Covers `core::fmt::FormattingOptions::align`
+#[test]
+fn test_formatting_options_align_none() {
+    let mut fo = fmt::FormattingOptions::new();
+    let alignment = None;
+    assert_eq!(fo.align(alignment).get_align(), alignment);
+}

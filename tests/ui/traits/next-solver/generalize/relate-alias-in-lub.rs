629ff9b4aa1f0216b26b44958d114332a793cf19
Jana Dönszelmann <jonathan@donsz.nl>
2026-02-02 12:48:42 +0100

    Update tests/ui/traits/next-solver/generalize/relate-alias-in-lub.rs
    Co-authored-by: lcnr <rust@lcnr.de>

--- tests/ui/traits/next-solver/generalize/relate-alias-in-lub.rs
+++ tests/ui/traits/next-solver/generalize/relate-alias-in-lub.rs
@@ -7,7 +7,7 @@
 // The change we tried to make there caused relating a type variable with an alias inside lub,
 // In 5bd20bbd0ba6c0285664e55a1ffc677d7487c98b, we moved around code
 // that adds an alias-relate predicate to be earlier, from one shared codepath into several
-// distinct code paths. However, we forgot one codepath, through lub, causing an ICE in serde.
+// distinct code paths. However, we forgot the codepath in `LatticeOp`, causing an ICE in serde.
 // In the end we dropped said commit, but the reproducer is still a useful as test.
 
 use std::marker::PhantomData;

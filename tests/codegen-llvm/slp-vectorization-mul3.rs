95ac5673cedf8b0ba543aac2ccb67b47f60662e1
ritik chahar <ritikchahar156@gmail.com>
2026-02-02 15:38:26 +0530

    Fix SLP vectorization test CHECK patterns

--- tests/codegen-llvm/slp-vectorization-mul3.rs
+++ tests/codegen-llvm/slp-vectorization-mul3.rs
@@ -4,23 +4,22 @@
 
 #![crate_type = "lib"]
 
-// CHECK-LABEL: mul3
-// CHECK: %[[C_BPP:.*]] = phi <4 x i8>
-// CHECK: %[[V:.*]] = load <4 x i8>, ptr
-// CHECK: %[[ADD:.*]] = add <4 x i8> %[[V]], %[[C_BPP]]
-// CHECK: store {{<4 x i8>|i32}} {{.*}}, ptr
+// CHECK-LABEL: @mul3
+// CHECK: phi <4 x i8>
+// CHECK: load <4 x i8>
+// CHECK: add <4 x i8>
+// CHECK: store <4 x i8>
 
-pub fn mul3(previous: &[u8], current: &mut [u8]) {
+#[no_mangle]
+pub fn mul3(previous: &[[u8; 4]], current: &mut [[u8; 4]]) {
     let mut c_bpp = [0u8; 4];
 
-    for (chunk, b_bpp) in current.chunks_exact_mut(4).zip(previous.chunks_exact(4)) {
-        let new_chunk = [
-            chunk[0].wrapping_add(c_bpp[0]),
-            chunk[1].wrapping_add(c_bpp[1]),
-            chunk[2].wrapping_add(c_bpp[2]),
-            chunk[3].wrapping_add(c_bpp[3]),
-        ];
-        chunk.copy_from_slice(&new_chunk);
-        c_bpp.copy_from_slice(b_bpp);
+    for i in 0..previous.len() {
+        current[i][0] = current[i][0].wrapping_add(c_bpp[0]);
+        current[i][1] = current[i][1].wrapping_add(c_bpp[1]);
+        current[i][2] = current[i][2].wrapping_add(c_bpp[2]);
+        current[i][3] = current[i][3].wrapping_add(c_bpp[3]);
+
+        c_bpp = previous[i];
     }
 }

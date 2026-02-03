67c6cd99b721309b32c3d4f484da4a856f32bef5
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2026-02-03 22:33:13 +0100

    Convert to inline diagnostics in `rustc_pattern_analysis`

--- compiler/rustc_driver_impl/src/lib.rs
+++ compiler/rustc_driver_impl/src/lib.rs
@@ -125,7 +125,6 @@ pub static DEFAULT_LOCALE_RESOURCES: &[&str] = &[
     rustc_mir_build::DEFAULT_LOCALE_RESOURCE,
     rustc_parse::DEFAULT_LOCALE_RESOURCE,
     rustc_passes::DEFAULT_LOCALE_RESOURCE,
-    rustc_pattern_analysis::DEFAULT_LOCALE_RESOURCE,
     rustc_trait_selection::DEFAULT_LOCALE_RESOURCE,
     // tidy-alphabetical-end
 ];

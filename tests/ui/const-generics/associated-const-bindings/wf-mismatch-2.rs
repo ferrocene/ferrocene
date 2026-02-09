ec03e39afd210cff8516b14b92dc259f61d303c5
zedddie <zedddie@protonmail.com>
2026-02-09 10:40:42 +0100

    bless tests

--- tests/ui/const-generics/associated-const-bindings/wf-mismatch-2.rs
+++ tests/ui/const-generics/associated-const-bindings/wf-mismatch-2.rs
@@ -1,12 +1,13 @@
-//@ known-bug: unknown
-//@ check-pass
+//! Check that we correctly handle associated const bindings
+//! in `dyn Trait` where the RHS is a const param (#151642).
 
 #![feature(min_generic_const_args)]
 #![expect(incomplete_features)]
 
-trait Trait { #[type_const] const CT: bool; }
+trait Trait { type const CT: bool; }
 
 fn f<const N: i32>() {
-    let _: dyn Trait<CT = { N }>; // FIXME: this should yield a type mismatch (`bool` v `i32`)
+    let _: dyn Trait<CT = { N }>;
+    //~^ ERROR the constant `N` is not of type `bool`
 }
 fn main() {}

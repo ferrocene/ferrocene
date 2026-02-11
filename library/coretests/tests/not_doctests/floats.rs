83d44c2777d37a4e8b89cb7edb04f56028fa0982
Urhengulas <johann.hemmann@ferrous-systems.com>
2026-02-11 16:35:34 +0100

    Cover `core::fmt::float::float_to_general_debug`

--- library/coretests/tests/not_doctests/floats.rs
+++ library/coretests/tests/not_doctests/floats.rs
@@ -4,3 +4,10 @@ fn test_f64_is_positive_negative() {
     assert!((1.0f64).is_positive());
     assert!((-1.0f64).is_negative());
 }
+
+// Cover `core::fmt::float::float_to_general_debug`
+#[test]
+fn float_to_general_debug_minus_plus() {
+    assert_eq!(format!("{:+?}", -10.0), "-10.0");
+    assert_eq!(format!("{:+?}", 10.0), "+10.0");
+}

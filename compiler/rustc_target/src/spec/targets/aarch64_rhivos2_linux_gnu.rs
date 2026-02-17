8f94b1821b0f332ec0f8b983595238a667c02f2a
Ana Hobden <operator@hoverbear.org>
2026-02-17 10:51:47 -0800

    Repair RHIVOS target metadata

--- compiler/rustc_target/src/spec/targets/aarch64_rhivos2_linux_gnu.rs
+++ compiler/rustc_target/src/spec/targets/aarch64_rhivos2_linux_gnu.rs
@@ -1,9 +1,9 @@
-use crate::spec::{Target, TargetMetadata};
+use crate::spec::Target;
 
 pub(crate) fn target() -> Target {
     let mut target = super::aarch64_unknown_linux_gnu::target();
 
-    target.metadata = TargetMetadata::default();
+    target.metadata.description = Some("ARM64 Linux (kernel 4.1, glibc 2.17+, RHIVOS)".into());
 
     target
 }

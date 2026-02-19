06cd82315877fa43ad9551870ad5ff57c1d02c64
Nicholas Nethercote <n.nethercote@gmail.com>
2026-02-19 14:27:28 +1100

    Rename some `Cache` type variables as `C`.
    Currently type variables that impl `QueryCache` are called either `C` or
    `Cache`. I find the former clearer because single char type variables
    names are very common and longer type variable names are easy to mistake
    for type or trait names -- e.g. I find the trait bound `C: QueryCache`
    much easier and faster to read than `Cache: QueryCache`.

--- compiler/rustc_query_impl/src/plumbing.rs
+++ compiler/rustc_query_impl/src/plumbing.rs
@@ -306,15 +306,15 @@ where
     QueryStackFrameExtra::new(description, span, def_kind)
 }
 
-pub(crate) fn create_deferred_query_stack_frame<'tcx, Cache>(
+pub(crate) fn create_deferred_query_stack_frame<'tcx, C>(
     tcx: TyCtxt<'tcx>,
-    vtable: &'tcx QueryVTable<'tcx, Cache>,
-    key: Cache::Key,
+    vtable: &'tcx QueryVTable<'tcx, C>,
+    key: C::Key,
 ) -> QueryStackFrame<QueryStackDeferred<'tcx>>
 where
-    Cache: QueryCache,
-    Cache::Key: Key + DynSend + DynSync,
-    QueryVTable<'tcx, Cache>: DynSync,
+    C: QueryCache,
+    C::Key: Key + DynSend + DynSync,
+    QueryVTable<'tcx, C>: DynSync,
 {
     let kind = vtable.dep_kind;
 

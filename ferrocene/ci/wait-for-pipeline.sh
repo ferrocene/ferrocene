528d0206a480b409dea619953ed7f1f497d227b6
Jynn Nelson <jynn.nelson@ferrous-systems.com>
2026-03-12 16:08:03 +0100

    Don't wait 6 hours to timeout if the `full` job never starts
    - Mark the `setup` job as required, not just the `full` job
    - Add a new `wait-for-pipeline` script that doesn't finish until it gets
      a success/error response from the CircleCI API
    - Mark the `setup` job as depending on `wait-for-pipeline`
    
    This uses a CircleCI "personal token" because project tokens only
    support API V1, not V2. V1 doesn't have the info we need, unfortunately.
    If I leave Ferrous, someone else will need to set up
    `CIRCLECI_API_TOKEN` in the environment.
    
    Similar, personal tokens cannot be scoped, so the jobs technically have
    write access to CircleCI. It might be possible to scope this only to the
    `setup` job using
    [contexts](https://circleci.com/docs/guides/security/contexts/) but I
    haven't looked into that.

---
+++ ferrocene/ci/wait-for-pipeline.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# SPDX-License-Identifier: MIT OR Apache-2.0
+# SPDX-FileCopyrightText: The Ferrocene Developers
+
+set -euo pipefail
+
+api_url="https://circleci.com/api/v2/pipeline/$CIRCLE_PIPELINE_ID/?circle-token=$CIRCLECI_API_TOKEN"
+i=0
+
+while ! [ "$i" = 12 ]; do
+    STATUS=$(curl --location --silent --dump-header /dev/stderr "$api_url" \
+        | { tee /dev/stderr; echo >>/dev/stderr; } \
+        | jq -r '.state')
+    case "$STATUS" in
+        created) exit 0;;
+        errored)
+            echo "pipeline errored! failing job."
+            exit 1
+            ;;
+        null|"") ## *something* went wrong
+            echo "internal error in 'wait-for-pipeline.sh'"
+            exit 2
+            ;;
+        *) : $((i++)); sleep 5;;
+    esac
+done
+
+echo "error: continutation workflow took more than a minute to start"
+exit 3

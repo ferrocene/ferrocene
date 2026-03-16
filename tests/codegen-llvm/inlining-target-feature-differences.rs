f9d7bb0791b002e778c4c2953210c5f80b0d38d9
Jynn Nelson <jynn.nelson@ferrous-systems.com>
2026-03-16 10:48:44 +0100

    Fix codegen test on thumbv7

--- tests/codegen-llvm/inlining-target-feature-differences.rs
+++ tests/codegen-llvm/inlining-target-feature-differences.rs
@@ -13,7 +13,7 @@ fn callee_neon(a: i32, b: i32) -> i32 {
 #[no_mangle]
 #[target_feature(enable = "neon")]
 pub fn caller_neon(x: i32, y: i32) -> i32 {
-    // CHECK-LABEL: define noundef i32 @caller_neon(
+    // CHECK-LABEL: define{{( dso_local)?}} noundef i32 @caller_neon(
     // CHECK: [[R:%[0-9A-Za-z_.]+]] = add
     callee_neon(x, y)
 }

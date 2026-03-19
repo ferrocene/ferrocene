ae33e8e5a830647d2bd9cc4b913eae8fb87b33fd
Urhengulas <johann.hemmann@ferrous-systems.com>
2026-03-19 11:51:41 +0100

    Update to LLVM 21

--- ferrocene/ci/scripts/setup-ubuntu.sh
+++ ferrocene/ci/scripts/setup-ubuntu.sh
@@ -11,10 +11,14 @@ sudo apt install -y \
     ninja-build
 
 if [[ ! -z "${INSTALL_LLVM}" ]]; then
+    # LLVM 21 is not available in the default ubuntu 24.04 repositories, so we have to get them from http://apt.llvm.org/noble/
+    wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | sudo tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc
+    echo "deb http://apt.llvm.org/noble/ llvm-toolchain-noble-21 main" | sudo tee /etc/apt/sources.list.d/llvm-21.list
+    sudo apt-get update
     sudo apt install -y \
-        llvm-20-tools \
-        llvm-20-dev \
-        libpolly-20-dev
+        llvm-21-tools \
+        llvm-21-dev \
+        libpolly-21-dev
 else
     echo 'Not installing LLVM, $INSTALL_LLVM is unset.'
 fi

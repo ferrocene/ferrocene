64cf22ba0ce40de68f7174c6e2c74db6dcf46c0b
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2026-04-01 08:27:02 +0200

    fix upstream pull conflicts

--- tests/ui/fn/issue-1900.rs
+++
@@ -1,6 +0,0 @@
-<<<PULL-UPSTREAM>>> file deleted upstream; move the Ferrocene annotations if any, and delete this file
-fn main<T>() { }
-//~^ ERROR `main` function is not allowed to have generic parameters
-
-// ferrocene-annotations: fls_8jb3sjqamdpu
-// Program Entry Point

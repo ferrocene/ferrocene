0f5a797c85ceaf08f94ad5640a420c6f68f873f9
Urhengulas <johann.hemmann@ferrous-systems.com>
2026-04-07 10:35:09 +0200

    Make panic message more specific to avoid accidental test success

--- library/coretests/tests/ferrocene/hint.rs
+++ library/coretests/tests/ferrocene/hint.rs
@@ -2,14 +2,14 @@
 /// `<core::hint::select_unpredictable::DropOnPanic<T> as core::ops::drop::Drop>::drop`
 /// is being executed, even if it is not shown as covered in the coverage report.
 #[test]
-#[should_panic = "0"]
+#[should_panic = "drop is reached"]
 fn test_select_unpredictable_runs_destructor() {
     struct Foo(i32);
 
     impl Drop for Foo {
         fn drop(&mut self) {
             if self.0 == 0 {
-                panic!("0");
+                panic!("drop is reached");
             }
         }
     }

b79e758402ce72412af236fdb93adf74b64bcd7a
Ana Hobden <operator@hoverbear.org>
2026-05-01 10:52:32 -0700

    Fix annotation on core::fmt::Formatter::<'a>::pad_formatted_parts

--- library/core/src/fmt/mod.rs
+++ library/core/src/fmt/mod.rs
@@ -2062,9 +2062,9 @@ impl<'a> Formatter<'a> {
             } else {
                 // Padding widths are capped at `u16`, so reaching this branch means
                 // the formatted output is also shorter than `u16::MAX`.
-                #[ferrocene::annotation("The `Err(_)` branch is explicitly marked unreachable, causing it be reached would be a bug")]
                 let len = match u16::try_from(len) {
                     Ok(len) => len,
+                    #[ferrocene::annotation("The `Err(_)` branch is explicitly marked unreachable, causing it be reached would be a bug")]
                     Err(_) => {
                         unreachable!()
                     },

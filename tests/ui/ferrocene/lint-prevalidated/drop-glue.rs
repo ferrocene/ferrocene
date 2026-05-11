73cc51505e42671a7240fe6a3c62d3e89be3809b
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2026-05-11 16:59:27 +0200

    state purpose of the test

--- tests/ui/ferrocene/lint-prevalidated/drop-glue.rs
+++ tests/ui/ferrocene/lint-prevalidated/drop-glue.rs
@@ -1,3 +1,5 @@
+// This test ensures that custom drop impls are caught by the lint.
+
 // Don't use anything from core here.
 // If you do, it will break once we start annotating more of core.
 // You can still use traits as long as the impls use custom types.

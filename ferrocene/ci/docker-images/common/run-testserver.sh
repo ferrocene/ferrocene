fa8671d56bd5412cd054cd5cc1a279e474ec8d39
Felix Gilcher <felix.gilcher@asquera.de>
2026-07-04 00:41:09 +0200

    we can't pass booleans to the container from circle
    so we'll need a string comparison

--- ferrocene/ci/docker-images/common/run-testserver.sh
+++ ferrocene/ci/docker-images/common/run-testserver.sh
@@ -15,6 +15,8 @@ QEMU_ARCH=$5
 env
 
 EMULATOR=""
-if $EMULATED; then EMULATOR=/opt/qemu-ferrocene/${QEMU_VERSION}/bin/${QEMU_ARCH}; fi
+if [[ "$EMULATED" == "true" ]];
+    then EMULATOR=/opt/qemu-ferrocene/${QEMU_VERSION}/bin/${QEMU_ARCH};
+fi
 ${EMULATOR} \
 /bin/test-server/$1/remote-test-server -v --bind $TEST_DEVICE_ADDR

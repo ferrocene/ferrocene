11e5555c8c31ccd0a28d7f397360124276be9983
Rachel Barker <rachel.barker@ferrous-systems.com>
2026-07-06 15:15:59 +0100

    Normalize out qemu abort message from more tests
    Fixes some test failures seen by Jonathan

--- tests/ui/panics/panic-resume_unwind-in-hook.rs
+++ tests/ui/panics/panic-resume_unwind-in-hook.rs
@@ -6,6 +6,9 @@
 //@ error-pattern: panicked while processing panic
 //@ ignore-emscripten "RuntimeError" junk in output
 
+// Ferrocene addition: Remove extra output line added by qemu
+//@ normalize-stderr: "qemu: uncaught target signal 6 \(Aborted\) - core dumped\n" -> ""
+
 use std::panic;
 
 fn main() {

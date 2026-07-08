d9c05e33cfc952a7296f43a4da95dec5cbe49974
Felix Gilcher <felix.gilcher@asquera.de>
2026-07-08 17:36:29 +0200

    don't build a per-run facade testserver image
    the image now only contains the ubuntu 24 image that we use in CI plus a very short script that waits for the facade testserver binary to show up in the expected location.

--- ferrocene/ci/docker-images/common/run-testserver.sh
+++ ferrocene/ci/docker-images/common/run-testserver.sh
@@ -3,10 +3,9 @@
 # SPDX-License-Identifier: MIT OR Apache-2.0
 # SPDX-FileCopyrightText: The Ferrocene Developers
 
-# call as run-testserver.sh <TARGET> <TEST_DEVICE_ADDR> <EMULATED> <QEMU_VERSION> <QEMU_ARCH> <QEMU_CPU>
+# call as run-testserver.sh <TEST_DEVICE_ADDR> <EMULATED> <QEMU_VERSION> <QEMU_ARCH> <QEMU_CPU>
 set -xe
 
-TARGET=$1
 TEST_DEVICE_ADDR=$2
 EMULATED=$3
 EMULATOR_VERSION=$4
@@ -15,9 +14,21 @@ EMULATOR_CPU=$6
 
 env
 
+until [ -e /shared/testserver/remote-test-server  ]; do
+  echo "Waiting until testserver binary is found in /shared/testserver/remote-test-server"
+  sleep 1
+done
+echo "Found testserver binary, proceeding"
+
+# Wait some in case copying takes a few seconds
+sleep 5
+
 EMULATOR=""
 if [[ "$EMULATED" == "true" ]]; then
     EMULATOR=/opt/qemu-ferrocene/${EMULATOR_VERSION}/bin/${EMULATOR_ARCH};
+    echo "Running under emulation"
+    echo "Emulator command: ${EMULATOR}"
+    echo "Emulator CPU: ${EMULATOR_CPU}"
 fi
 
 if [[ "" != "$EMULATOR_CPU" ]]; then
@@ -25,4 +36,4 @@ if [[ "" != "$EMULATOR_CPU" ]]; then
 fi
 
 ${EMULATOR} \
-/bin/test-server/$1/remote-test-server -v --bind $TEST_DEVICE_ADDR
+/shared/testserver/remote-test-server -v --bind $TEST_DEVICE_ADDR

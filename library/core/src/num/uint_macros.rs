3efd912bbdc1577f0461a919d9896409b98a2100
Felix Gilcher <felix.gilcher@asquera.de>
2026-07-09 15:10:11 +0200

    turn off more should_panic tests

--- library/core/src/num/uint_macros.rs
+++ library/core/src/num/uint_macros.rs
@@ -1333,7 +1333,8 @@ macro_rules! uint_impl {
         ///
         /// The following panics because of overflow:
         ///
-        /// ``` should_panic
+        #[cfg_attr(ferrocene_facade_secretsauce, doc = "```no_run")]
+        #[cfg_attr(not(ferrocene_facade_secretsauce), doc = "```should_panic")]
         #[doc = concat!("let _ = ", stringify!($SelfT), "::MAX.strict_mul(2);")]
         /// ```
         #[stable(feature = "strict_overflow_ops", since = "1.91.0")]

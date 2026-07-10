f547c52755786a967d5eb2aab41dec3e47832e13
Carmen Donnelly <carmen.donnelly@ferrous-systems.com>
2026-07-10 16:40:09 +0200

    export env vars in command instead of script

--- ferrocene/ci/scripts/use-squid-cache.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# SPDX-License-Identifier: MIT OR Apache-2.0
-# SPDX-FileCopyrightText: The Ferrocene Developers
-
-set -v
-
-export http_proxy="http://squid.ci-squid-cache.svc.cluster.local:3148"
-export https_proxy="https://squid.ci-squid-cache.svc.cluster.local:4148"

49bd05bce699c4c7eea4def1687a162ac65fbe97
jyn <github@jyn.dev>
2026-07-13 12:54:20 +0200

    automation: distinguish versions if an automated PR is open across a version bump
    currently, we can get in the following awful situation:
    1. `pull-upstream` opens a PR from upstream `stable` to `release/1.96`
    2. that PR remains unmerged for a few weeks
    3. upstream releases `1.97` stable
    4. automation reruns, notices that an existing PR is open, and refuses
       to open a PR to `release/1.97`.
    
    this is because the automation's cache key is the PR title and labels,
    neither of which contain the target branch. add it to the title so it
    invalidates the cache.

--- ferrocene/tools/automations-common/src/automations_common/automated_prs.py
+++ ferrocene/tools/automations-common/src/automations_common/automated_prs.py
@@ -116,7 +116,7 @@ class AutomatedPR(abc.ABC):
         self.current_branch = self.cmd_capture(["git", "branch", "--show-current"])
         self.current_hash = self.cmd_capture(["git", "rev-parse", "HEAD"])
 
-        existing_pull = self.__find_open("pulls", self.pr_title(), self.pr_labels())
+        existing_pull = self.__find_open("pulls", self.__resolved_pr_title(), self.pr_labels())
         if existing_pull is not None:
             log("An automated PR is already open, a new one won't be created.")
             log(f"==> {existing_pull['html_url']}")
@@ -159,7 +159,7 @@ class AutomatedPR(abc.ABC):
             response = self.http.post(
                 self.__repo_api("pulls"),
                 json={
-                    "title": self.pr_title(),
+                    "title": self.__resolved_pr_title(),
                     "head": branch_name,
                     "base": self.base_branch(),
                     "body": self.pr_body(branch_name),
@@ -287,6 +287,12 @@ class AutomatedPR(abc.ABC):
     #     Internals     #
     #                   #
     #####################
+
+    def __resolved_pr_title(self):
+        if self.base_branch() == 'main':
+            return self.pr_title()
+        version = self.base_branch().removeprefix("release/")
+        return f"[{version}] {self.pr_title()}"
 
     def __find_open(self, kind, expected_title, expected_labels):
         """

5539825f3521e87454b65aae94b37d9d2149f5a6
Felix Gilcher <felix.gilcher@asquera.de>
2026-07-15 00:52:24 +0200

    read awscli version from a file

--- ferrocene/ci/scripts/calculate_parameters.py
+++ ferrocene/ci/scripts/calculate_parameters.py
@@ -245,9 +245,9 @@ def workflow_id(*dummy):
     return os.environ.get("CIRCLE_WORKFLOW_ID")
 
 
-# This needs to be kept in sync with the version in ferrocene/ci/docker-images/common/install-awscli.sh
+# read from ferrocene/ci/awscli-version
 def awscli_version(*dummy):
-    return "2.35.11"
+    return Path('../awscli-version').read_text()
 
 
 def prepare_parameters():

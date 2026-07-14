0537d61ebf792aceaa9bb8d6494e203bc921d1bf
Felix Gilcher <felix.gilcher@asquera.de>
2026-07-15 01:25:27 +0200

    build docker images with the version recorded in ferrocene/ci/awscli-version

--- ferrocene/ci/docker-images/common/setup-awscli.sh
+++ ferrocene/ci/docker-images/common/setup-awscli.sh
@@ -5,24 +5,64 @@
 
 rm -rf /tmp/awscli /tmp/awscli.zip
 set -xe
+
+mkdir /tmp/awscli && cd /tmp/awscli
+
+echo "-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mQINBF2Cr7UBEADJZHcgusOJl7ENSyumXh85z0TRV0xJorM2B/JL0kHOyigQluUG
+ZMLhENaG0bYatdrKP+3H91lvK050pXwnO/R7fB/FSTouki4ciIx5OuLlnJZIxSzx
+PqGl0mkxImLNbGWoi6Lto0LYxqHN2iQtzlwTVmq9733zd3XfcXrZ3+LblHAgEt5G
+TfNxEKJ8soPLyWmwDH6HWCnjZ/aIQRBTIQ05uVeEoYxSh6wOai7ss/KveoSNBbYz
+gbdzoqI2Y8cgH2nbfgp3DSasaLZEdCSsIsK1u05CinE7k2qZ7KgKAUIcT/cR/grk
+C6VwsnDU0OUCideXcQ8WeHutqvgZH1JgKDbznoIzeQHJD238GEu+eKhRHcz8/jeG
+94zkcgJOz3KbZGYMiTh277Fvj9zzvZsbMBCedV1BTg3TqgvdX4bdkhf5cH+7NtWO
+lrFj6UwAsGukBTAOxC0l/dnSmZhJ7Z1KmEWilro/gOrjtOxqRQutlIqG22TaqoPG
+fYVN+en3Zwbt97kcgZDwqbuykNt64oZWc4XKCa3mprEGC3IbJTBFqglXmZ7l9ywG
+EEUJYOlb2XrSuPWml39beWdKM8kzr1OjnlOm6+lpTRCBfo0wa9F8YZRhHPAkwKkX
+XDeOGpWRj4ohOx0d2GWkyV5xyN14p2tQOCdOODmz80yUTgRpPVQUtOEhXQARAQAB
+tCFBV1MgQ0xJIFRlYW0gPGF3cy1jbGlAYW1hem9uLmNvbT6JAlQEEwEIAD4CGwMF
+CwkIBwIGFQoJCAsCBBYCAwECHgECF4AWIQT7Xbd/1cEYuAURraimMQrMRnJHXAUC
+ZqFYbwUJCv/cOgAKCRCmMQrMRnJHXKYuEAC+wtZ611qQtOl0t5spM9SWZuszbcyA
+0xBAJq2pncnp6wdCOkuAPu4/R3UCIoD2C49MkLj9Y0Yvue8CCF6OIJ8L+fKBv2DI
+yWZGmHL0p9wa/X8NCKQrKxK1gq5PuCzi3f3SqwfbZuZGeK/ubnmtttWXpUtuU/Iz
+VR0u/0sAy3j4uTGKh2cX7XnZbSqgJhUk9H324mIJiSwzvw1Ker6xtH/LwdBeJCck
+bVBdh3LZis4zuD4IZeBO1vRvjot3Oq4xadUv5RSPATg7T1kivrtLCnwvqc6L4LnF
+0OkNysk94L3LQSHyQW2kQS1cVwr+yGUSiSp+VvMbAobAapmMJWP6e/dKyAUGIX6+
+2waLdbBs2U7MXznx/2ayCLPH7qCY9cenbdj5JhG9ibVvFWqqhSo22B/URQE/CMrG
++3xXwtHEBoMyWEATr1tWwn2yyQGbkUGANneSDFiTFeoQvKNyyCFTFO1F2XKCcuDs
+19nj34PE2TJilTG2QRlMr4D0NgwLLAMg2Los1CK6nXWnImYHKuaKS9LVaCoC8vu7
+IRBik1NX6SjrQnftk0M9dY+s0ZbAN1gbdjZ8H3qlbl/4TxMdr87m8LP4FZIIo261
+Eycv34pVkCePZiP+dgamEiQJ7IL4ZArio9mv6HbDGV6mLY45+l6/0EzCwkI5IyIf
+BfWC9s/USgxchg==
+=ptgS
+-----END PGP PUBLIC KEY BLOCK-----" > aws_cli_public.key
+
+gpg --import aws_cli_public.key
+
 if [ "$TARGETPLATFORM" = "linux/amd64" ]; then
     ARCH="x86_64"
-    SHA="d4bbcb5532c8bf43f9149f1b1e0d09f77f388df088656e349570637c06b76d2d"
 elif [ "$TARGETPLATFORM" = "linux/arm64" ]; then
     ARCH="aarch64"
-    SHA="df18f41e85b1305747a51b2da3886dd17ecde7412614068ed19905d6b245c69b"
 else
     echo "Unsupported platform"
     exit 1
 fi
-# The version defined here needs to be kept in sync with the version defined in
-# ferrocene/ci/awscli-version
-PACKAGE_URL="https://awscli.amazonaws.com/awscli-exe-linux-${ARCH}-2.35.11.zip"
-curl -Lo /tmp/awscli.zip $PACKAGE_URL
-echo "${SHA} /tmp/awscli.zip" | sha256sum -c
-unzip -q -d /tmp/awscli /tmp/awscli.zip
+# The version defined here is read from ferrocene/ci/awscli-version
+# This is executed in docker, and the version file should be copied to the expected
+# location by the dockerfile
+
+VERSION=$(cat /tmp/awscli-version)
+PACKAGE_URL="https://awscli.amazonaws.com/awscli-exe-linux-${ARCH}-${VERSION}.zip"
+
+curl -Lo awscliv2.sig "${PACKAGE_URL}.sig"
+curl -Lo "awscliv2.zip" $PACKAGE_URL
+gpg --verify awscliv2.sig awscliv2.zip
+
+unzip -q -d /tmp/awscli awscliv2.zip
 # we want to update any install previously found
 /tmp/awscli/aws/install --update
+rm -rf /tmp/awscli awscliv2.zip awscliv2.sig
 
 # some of our environments record all downloaded packages here
 if [ test -x "/ferrocene/packages" ]; then

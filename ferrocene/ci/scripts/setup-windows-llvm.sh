1c30c8dbdee8b88e7a3d6a3db1c9ab73eda71c88
Felix Gilcher <felix.gilcher@asquera.de>
2026-07-14 22:26:02 +0200

    only set up llvm build deps if needed

---
+++ ferrocene/ci/scripts/setup-windows-llvm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# SPDX-License-Identifier: MIT OR Apache-2.0
+# SPDX-FileCopyrightText: The Ferrocene Developers
+
+set -xeuo pipefail
+IFS=$'\n\t'
+
+choco install -y llvm
+
+# Followed: https://docs.chocolatey.org/en-us/guides/create/recompile-packages/#how-to-internalizerecompile-an-existing-package-manually
+# From: https://developer.arm.com/-/media/Files/downloads/gnu/15.2.rel1/binrel/arm-gnu-toolchain-15.2.rel1-mingw-w64-x86_64-arm-none-eabi.msi
+aws s3 cp s3://ferrocene-ci-mirrors/manual/arm-compiler/gcc-arm-embedded.10.3.1.20251211.nupkg gcc-arm-embedded.10.3.1.20251211.nupkg
+choco install gcc-arm-embedded --version="10.3.1.20251211" --source .

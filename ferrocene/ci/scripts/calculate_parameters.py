f98847544f8c42667254e71ec24bff68d2af35a6
Felix Gilcher <felix.gilcher@asquera.de>
2026-08-12 09:43:48 +0200

    Backport ferrocene/ferrocene#2446
    use docker executors for embedded targets
    
    This uses the docker executor for facade tests, employing the same strategy that we use for the rhivos tests.
    
    This PR contains the changes from https://github.com/ferrocene/ferrocene/pull/2484 rewritten to match the new facade test setup.
    
    This change updates QEMU to a recent version as part of the setup work.
    
    Switch the `x86_64-linux-generic-test-container` container to a platform-independent `linux-generic-test-container`. CircleCI does not support nested containers, so we cannot preserve the existing `x86_64-linux-generic-test-container`.
    
    This also switches `linux-generic-test-container` from a `ferrocene/k8s-amd64-large` to `medium` runner, since our jobs hardly ever used the resources of the larger runner.
    
    `should_panic` doctests for`panic=abort` platforms cannot be run through remote-testserver and have been disabled. This is a current limitation of remote-testserver. Doctests are run opportunistic and are  not qualification-relevant, so the slight reduction in tests is not harmful.Co-authored-by: {author}
    
    Ferrocene-backport-of: ferrocene/ferrocene#2446
    Ferrocene-backported-commits: 7aafd07aa8e0a5f74909233e0ce0ad9a4ebec464 2a6c3f7e62b1547778e7b062ce5a634b77f2b78a 7017ccf06b08cf546376ad64348bea24864156af e290aaa53066a31fb32daa1a878fdba53510f1ad e1745157285069a8812440ecdccbee28b170ac90 8641e4719ddeff35955d3b40cd2df5b818caa82d b7e38bae0d879d331260368c42b62741ff45c5f6 8beadf597543c28a2a9bada5da5233e8d8f3f02d 3edb0399c0718b3ddbeb4a27f2fa87b81e649de0 3d15ed24d53a3968bc0b4a9c186ad46dd3fda64f c627329a6478d5b35c56c3aac797dc8ea7757955 f57092f6bf59f8e6a77208ec9e7f2e07b9099f7e f8c9a950a552072d2fa34f0c9204f8765bc72ffb a75ef948bea912554729d235a9a844c3be7eba87 a8c8cb7ed2fad815626c2dc5181c62c691089f8c 58b14c94c0c1c21f41d14af6c9444214354624b9 69d8f9d290d25e6854e48542f561553a6b11af3e 8574c0d581c21282d53b9f140c6e700413ebd662 aedbff7434aa350bad8251fef749f562b63dda11 f6c9ebc90cec877f71dfd18730696c7d3879f975 74325d52b28293c230ca5a647a505a1de4f378a2 f2583e048218a2d48814ed1dc3719c690ec35e24 2c016b88d9abe80c0b61497c526ed1b0400f5602 708ff2ad15852500a83a2c8f50907eab2eef472c bd05a8078a19831b3995183ffacdba4b65918601 39e1c7308a6e1ef86e6b65f513220e3992b96d8f 23a167cba5a771258bd8a925f72979a2d24c9aab b7fd528ceab5829b9f307cb71c87ca479367e4d0 5963c40b9ef4f51a5ea42633d6a5c0abf346b6fe d68756b675d0a977f464053543a360611ffc6948 f6be02cd58574687ce268f2fef885e16937a4dda 307a9a181895f8cb58aafb1fa336c9855beec8ab 487a93ce4622c942cf5a4427fded4acc9387bf41 8100ded32a95e4438ba48b665bcde19e23c750a8 d9c05e33cfc952a7296f43a4da95dec5cbe49974 89fe8f7adb621067d4810b6081e65a818512af0a 4991b5f96c450b6d236a9895f11b85bfb8bf1e36 c6b7e36207cf8db43aca6a20add5c8f9a69aa5d0 c71e33fd5f217630ef0d74d3d56208603340232e fb1724580b58017f3efaac42291cabf4bd5edc45 812cc489ed4fdd430e25b246e9bf94879e1f22b7 631bd59a7a3f679f46f7cfb31037f5556cfc1781

--- ferrocene/ci/scripts/calculate_parameters.py
+++ ferrocene/ci/scripts/calculate_parameters.py
@@ -226,6 +226,11 @@ def awscli_version(*dummy):
     return Path("ferrocene/ci/awscli-version").read_text()
 
 
+# read from ferrocene/ci/qemu-version
+def qemu_version(*dummy):
+    return Path("ferrocene/ci/qemu-version").read_text()
+
+
 def prepare_parameters():
     replacements: dict[str, Callable[[str], str | bool]] = {
         "docker-images-hash": lambda _: docker_images.calculate_hash(),
@@ -234,6 +239,7 @@ def prepare_parameters():
         "targets--": calculate_targets,
         "stable-workflow-id": workflow_id,
         "awscli-version": awscli_version,
+        "qemu-version": qemu_version,
     }
 
     parameters: dict[str, str | bool] = {}

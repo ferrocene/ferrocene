2572e95d9f87eebc15252bf8a915a11631478dc8
Ana Hobden <operator@hoverbear.org>
2026-08-14 10:28:32 -0700

    Cover `core::fmt::builders::DebugMap::<'a, 'b>::finish_non_exhaustive`'s try branches

--- library/coretests/tests/ferrocene/fmt.rs
+++ library/coretests/tests/ferrocene/fmt.rs
@@ -1313,3 +1313,30 @@ fn test_debug_list_finish_non_exhaustive_try_branches() {
     };
     assert!(write!(writer, "{:#?}", Foo(vec![1, 2, 3, 4])).is_err());
 }
+
+// Cover `core::fmt::builders::DebugMap::<'a, 'b>::finish_non_exhaustive`'s try branches
+#[test]
+fn test_debug_map_finish_non_exhaustive_try_branches() {
+    use std::fmt;
+
+    struct Foo(Vec<(char, i32)>);
+
+    impl fmt::Debug for Foo {
+        fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+            // Print at most two elements, abbreviate the rest
+            let mut f = fmt.debug_map();
+            let f = f.entries(self.0.iter().take(2).map(|&(ref k, ref v)| (k, v)));
+            if self.0.len() > 2 {
+                f.finish_non_exhaustive()
+            } else {
+                f.finish()
+            }
+        }
+    }
+
+    let mut writer = ErrorTriggerWriter {
+        error_on: "..\n",
+        allow_times: 0,
+    };
+    assert!(write!(writer, "{:#?}", Foo(vec![('a', 1), ('b', 2), ('c', 3), ('d', 4)])).is_err());
+}

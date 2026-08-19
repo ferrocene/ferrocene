e708c79af62546f0359febf8ea7e50cdc359659d
Urhengulas <johann.hemmann@ferrous-systems.com>
2026-08-19 13:13:43 +0200

    Add `ferrocene::requires_validation` attribute

--- compiler/rustc_middle/src/middle/codegen_fn_attrs/ferrocene.rs
+++ compiler/rustc_middle/src/middle/codegen_fn_attrs/ferrocene.rs
@@ -15,6 +15,7 @@ pub struct Validated {
 }
 
 const VALIDATED_ATTR: &[Symbol; 2] = &[sym::ferrocene, sym::prevalidated];
+const REQUIRES_VALIDATION_ATTR: &[Symbol; 2] = &[sym::ferrocene, sym::requires_validation];
 
 #[derive(Debug)]
 pub enum ValidatedStatus {

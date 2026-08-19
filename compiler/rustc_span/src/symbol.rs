51157352658cb5ce33c5e093f3a6d4af680b75d5
Urhengulas <johann.hemmann@ferrous-systems.com>
2026-08-19 13:13:43 +0200

    Add `ferrocene::requires_validation` attribute

--- compiler/rustc_span/src/symbol.rs
+++ compiler/rustc_span/src/symbol.rs
@@ -1716,6 +1716,7 @@ symbols! {
         repr_simd,
         repr_transparent,
         require,
+        requires_validation,
         reserve_x18: "reserve-x18",
         residual,
         result,

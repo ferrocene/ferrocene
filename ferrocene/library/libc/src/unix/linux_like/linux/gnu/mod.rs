d3ff5ae40247118e6e6818ffc10123ace2255e71
Tshepang Mbambo <tshepang.mbambo@ferrous-systems.com>
2026-09-22 17:50:22 +0200

    adjust to new lint, repeated_reprs

--- ferrocene/library/libc/src/unix/linux_like/linux/gnu/mod.rs
+++ ferrocene/library/libc/src/unix/linux_like/linux/gnu/mod.rs
@@ -459,7 +459,6 @@ impl siginfo_t {
 s_no_extra_traits! {
     // linux/if_ether.h
 
-    #[repr(C, packed)]
     pub struct ethhdr {
         pub h_dest: [c_uchar; crate::ETH_ALEN as usize],
         pub h_source: [c_uchar; crate::ETH_ALEN as usize],
